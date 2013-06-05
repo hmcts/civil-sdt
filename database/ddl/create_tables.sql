fcf593529c7eda3b5a29c553a83b3a467b727b79
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-05 10:33:31 +0000

    System Task #3266 Updated following review by RC/SA
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16444 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_tables.sql
+++ database/ddl/create_tables.sql
@@ -1,18 +1,18 @@
 connect sdt_owner/sdt_owner
 
 
-define bulk_customers          = 'TABLESPACE users'
-define bulk_customer_services  = 'TABLESPACE users'
-define bulk_submissions        = 'TABLESPACE users'
-define bulk_submissions_lob    = 'LOB (bulk_payload) STORE AS bs_lob(TABLESPACE users)'
-define error_log               = 'TABLESPACE users'
-define error_messages          = 'TABLESPACE users'
-define global_parameters       = 'TABLESPACE users'
-define individual_requests     = 'TABLESPACE users'
-define individual_requests_lob = 'LOB (individual_payload) STORE AS is_lob(TABLESPACE users)'
-define request_types           = 'TABLESPACE users'
-define routing_table           = 'TABLESPACE users'
-define valid_services          = 'TABLESPACE users'
+define bulk_customers              = 'TABLESPACE users'
+define bulk_customer_applications  = 'TABLESPACE users'
+define bulk_submissions            = 'TABLESPACE users'
+define bulk_submissions_lob        = 'LOB (bulk_payload) STORE AS bs_lob(TABLESPACE users)'
+define error_log                   = 'TABLESPACE users'
+define error_messages              = 'TABLESPACE users'
+define global_parameters           = 'TABLESPACE users'
+define individual_requests         = 'TABLESPACE users'
+define individual_requests_lob     = 'LOB (individual_payload) STORE AS is_lob(TABLESPACE users)'
+define request_types               = 'TABLESPACE users'
+define request_routings            = 'TABLESPACE users'
+define target_applications         = 'TABLESPACE users'
 
 
 CREATE TABLE bulk_customers
@@ -20,52 +20,62 @@ CREATE TABLE bulk_customers
 ,bulk_customer_case_code  CHAR(2)      
 ,sdt_customer_id          NUMBER(4,0)
 ,customer_identifier      NUMBER(4,0)     -- between 1000 and 9999
+,version_number           INTEGER         -- hiberate versioning column
 ) &bulk_customers
 ;
 
 
-CREATE TABLE bulk_customer_services
+CREATE TABLE bulk_customer_applications
 (bulk_customer_id         INTEGER         -- pk, fk from bulk_customers
-,valid_service_id         CHAR(4)         -- mcol, pcol,c_mc, c_pc
-) &bulk_customer_services
+,target_application_id    INTEGER         -- pk, fk from target_applications
+,customer_application_id  VARCHAR2(32)   
+,version_number           INTEGER         -- hiberate versioning column
+) &bulk_customer_applications
 ;
 
 CREATE TABLE bulk_submissions
 (bulk_submission_id       INTEGER         -- pk
 ,bulk_customer_id         INTEGER         -- fk from bulk_customers
-,valid_service_id         CHAR(4)         -- fk from valid_services
+,target_application_id    INTEGER         -- fk from target_applications
 ,sdt_bulk_reference       VARCHAR2(32)    -- fixed format 
-,user_file_ref            VARCHAR2(32)
-,submitted_date           TIMESTAMP
+,customer_reference       VARCHAR2(32)
+,created_date             TIMESTAMP       -- date/time of record created
 ,number_of_requests       INTEGER
 ,bulk_submission_status   VARCHAR2(20)
 ,completed_date           TIMESTAMP
-,updated_date             TIMESTAMP
-,bulk_payload             clob
+,updated_date             TIMESTAMP       -- date/time of last change to record
+,version_number           INTEGER         -- hiberate versioning column
+,bulk_payload             BLOB
 ) &bulk_submissions_lob
 &bulk_submissions
 ;
 
 CREATE TABLE error_log
-(error_log_id             INTEGER        -- pk, synthetic
-,individual_request_id    INTEGER        -- fk from individual_requests
-,error_code               INTEGER        -- fk from error_messages
-,error_locator            INTEGER        -- ref to error in xml
-,error_text               VARCHAR2(1000) -- detailed error mesg
+(error_log_id             INTEGER         -- pk, synthetic
+,bulk_submission_id       INTEGER         -- fk from bulk_submissions
+,individual_request_id    INTEGER         -- fk from individual_requests, null for error raised on bulk file 
+,error_message_id         INTEGER         -- fk from error_messages
+,created_date             TIMESTAMP       -- date/time of record created
+,updated_date             TIMESTAMP       -- date/time of last change to record
+,version_number           INTEGER         -- hiberate versioning column
+,error_text               VARCHAR2(1000)  -- detailed error mesg
 ) &error_log
 ;
 
 CREATE TABLE error_messages
-(error_code               INTEGER        -- pk, synthetic
-,error_text               VARCHAR2(1000) -- error mesg with placeholder for context
-,error_description        VARCHAR2(2000) -- generic description of error
+(error_message_id         INTEGER         -- pk, synthetic
+,error_code               VARCHAR2(32)    -- 
+,error_text               VARCHAR2(1000)  -- error mesg with placeholder for context
+,error_description        VARCHAR2(2000)  -- generic description of error
+,version_number           INTEGER         -- hiberate versioning column
 ) &error_messages
 ;
 
 CREATE TABLE global_parameters
-(parameter_id             INTEGER
+(global_parameter_id      INTEGER
 ,parameter_name           VARCHAR2(32)
 ,parameter_value          VARCHAR2(32)
+,version_number           INTEGER         -- hiberate versioning column
 ) &global_parameters
 ;
 
@@ -73,14 +83,23 @@ CREATE TABLE individual_requests
 (individual_request_id    INTEGER        -- pk
 ,bulk_submission_id       INTEGER        -- fk from bulk_submissions
 ,request_type_id          INTEGER        -- fk from request_type
-,user_request_reference   VARCHAR2(32)
+,customer_request_ref     VARCHAR2(32)
 ,case_number              VARCHAR2(32)
 ,request_status           VARCHAR2(32)
 ,issued_date              TIMESTAMP
 ,sdt_bulk_reference       VARCHAR2(32)
 ,line_number              INTEGER
-,sdt_request_reference    VARCHAR2(38) -- fixed format
-,individual_payload       clob
+,sdt_request_reference    VARCHAR2(38)   -- fixed format
+,created_date             TIMESTAMP      -- date/time of record created
+,updated_date             TIMESTAMP      -- date/time of last change to record
+,completed_date           TIMESTAMP
+,service_date             TIMESTAMP
+,warrant_number           INTEGER
+,enforcing_court_code     VARCHAR2(32)
+,enforcing_court_name     VARCHAR2(255)
+,fee                      NUMBER(8,2)
+,version_number           INTEGER         -- hiberate versioning column
+,individual_payload       BLOB
 ) &individual_requests_lob 
 &individual_requests 
 ;
@@ -88,19 +107,25 @@ CREATE TABLE individual_requests
 CREATE TABLE request_types
 (request_type_id          INTEGER
 ,request_type_name        VARCHAR2(50)
+,request_type_status      VARCHAR2(1)
+,request_type_description VARCHAR2(2000)
+,version_number           INTEGER         -- hiberate versioning column
 ) &request_types
 ;
 
-CREATE TABLE routing_table
+CREATE TABLE request_routings
 (request_type_id          INTEGER        -- pk, fk to request_types
-,valid_service_id         CHAR(4)        -- pk, fk to valid_services
-,web_service_name         VARCHAR2(32)
-) &routing_table
+,target_application_id    INTEGER        -- pk, fk to valid_services
+,web_service_endpoint     VARCHAR2(255)
+,version_number           INTEGER         -- hiberate versioning column
+) &request_routings
 ;
 
-CREATE TABLE valid_services
-(valid_service_id         CHAR(4)         -- pk, mcol, pcol,c_mc, c_pc
-,valid_service_name       VARCHAR2(30)
-) &valid_services
+CREATE TABLE target_applications
+(target_application_id    INTEGER         -- pk
+,target_application_code  VARCHAR2(4)     -- mcol, pcol,c_mc, c_pc
+,target_application_name  VARCHAR2(255)
+,version_number           INTEGER         -- hiberate versioning column
+) &target_applications
 ;
 
