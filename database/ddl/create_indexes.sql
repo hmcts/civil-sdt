06819b345a84a01932fe96336b753e19b4220fa8
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-05 10:32:45 +0000

    System Task #3266 Updated following review by RC/SA
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16441 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_indexes.sql
+++ database/ddl/create_indexes.sql
@@ -1,15 +1,41 @@
 connect sdt_owner/sdt_owner
 
 
-DEFINE bs_sdt_bulk_reference_i = 'TABLESPACE users'
-DEFINE ir_bulk_reference_i     = 'TABLESPACE users'
+DEFINE bs_sdt_bulk_reference_i       = 'TABLESPACE users'
+DEFINE ir_bulk_reference_i           = 'TABLESPACE users'
+DEFINE rt_request_type_name_i        = 'TABLESPACE users'
+DEFINE ta_target_application_name_i  = 'TABLESPACE users'
+
+--
+-- bulk_submissions
+--
 
 CREATE INDEX bs_sdt_bulk_reference_i
 ON bulk_submissions ( sdt_bulk_reference )
 &bs_sdt_bulk_reference_i
 ;
 
+--
+--individual_requests
+--
+
 CREATE INDEX ir_bulk_reference_i
 ON individual_requests ( sdt_bulk_reference )
 &ir_bulk_reference_i
+;
+
+--
+-- request_types
+--
+CREATE INDEX rt_request_type_name
+ON request_types ( request_type_name )
+&rt_request_type_name_i
+;
+
+-- 
+-- target_applications
+--
+CREATE INDEX ta_target_application_name 
+ON target_applications ( target_application_name)
+&ta_target_application_name_i
 ;
