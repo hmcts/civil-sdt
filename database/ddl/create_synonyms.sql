f832677c18706594eb7c89c941fa9f48de03dea2
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-05 10:33:15 +0000

    System Task #3266 Updated following review by RC/SA
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16443 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_synonyms.sql
+++ database/ddl/create_synonyms.sql
@@ -1,13 +1,13 @@
 connect sdt_user/sdt_user
 
-CREATE SYNONYM bulk_customers         FOR sdt_owner.bulk_customers;       
-CREATE SYNONYM bulk_customer_services FOR sdt_owner.bulk_customer_services;
-CREATE SYNONYM bulk_submissions       FOR sdt_owner.bulk_submissions;
-CREATE SYNONYM error_log              FOR sdt_owner.error_log;
-CREATE SYNONYM error_messages         FOR sdt_owner.error_messages;
-CREATE SYNONYM global_parameters      FOR sdt_owner.global_parameters;
-CREATE SYNONYM individual_requests    FOR sdt_owner.individual_requests;
-CREATE SYNONYM request_types          FOR sdt_owner.request_types;
-CREATE SYNONYM routing_table          FOR sdt_owner.routing_table;
-CREATE SYNONYM valid_services         FOR sdt_owner.valid_services;
+CREATE SYNONYM bulk_customers             FOR sdt_owner.bulk_customers;       
+CREATE SYNONYM bulk_customer_applications FOR sdt_owner.bulk_customer_applications;
+CREATE SYNONYM bulk_submissions           FOR sdt_owner.bulk_submissions;
+CREATE SYNONYM error_log                  FOR sdt_owner.error_log;
+CREATE SYNONYM error_messages             FOR sdt_owner.error_messages;
+CREATE SYNONYM global_parameters          FOR sdt_owner.global_parameters;
+CREATE SYNONYM individual_requests        FOR sdt_owner.individual_requests;
+CREATE SYNONYM request_types              FOR sdt_owner.request_types;
+CREATE SYNONYM request_routings           FOR sdt_owner.requst_routings;
+CREATE SYNONYM target_applications        FOR sdt_owner.target_applications;
 
