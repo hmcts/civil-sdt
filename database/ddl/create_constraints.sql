2ed449a17906a98fa49b28c88473977f641122d8
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-05 10:30:07 +0000

    System Task #3266 Updated following review by RC/SA
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16437 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_constraints.sql
+++ database/ddl/create_constraints.sql
@@ -1,6 +1,28 @@
 connect sdt_owner/sdt_owner
 
+--
+-- bulk_submissions
+--
+
 ALTER TABLE bulk_submissions    ADD CONSTRAINT bs_sbr_nn CHECK (sdt_bulk_reference IS NOT NULL);
 ALTER TABLE bulk_submissions    ADD CONSTRAINT bs_sbr_uni UNIQUE (sdt_bulk_reference );
+
+
+--
+-- individual_requests
+--
+
 ALTER TABLE individual_requests ADD CONSTRAINT ir_sbr_nn CHECK (sdt_bulk_reference IS NOT NULL);
 
+--
+-- request_types
+--
+
+ALTER TABLE request_types       ADD CONSTRAINT rt_rtn_uni UNIQUE (request_type_name) ;
+
+
+--
+--target_applications
+--
+
+ALTER TABLE target_applications ADD CONSTRAINT ta_target_application_name_uni UNIQUE (target_application_name) ;
