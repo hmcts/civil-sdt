a61e187740064bdb22aad1409571ef6a2d79e488
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-10 14:08:52 +0000

    System Task #3266 adding message_log and correcting error_logs
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16471 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_synonyms.sql
+++ database/ddl/create_synonyms.sql
@@ -3,10 +3,11 @@ alter session set current_schema=sdt_user;
 CREATE SYNONYM bulk_customers             FOR sdt_owner.bulk_customers;       
 CREATE SYNONYM bulk_customer_applications FOR sdt_owner.bulk_customer_applications;
 CREATE SYNONYM bulk_submissions           FOR sdt_owner.bulk_submissions;
-CREATE SYNONYM error_log                  FOR sdt_owner.error_log;
+CREATE SYNONYM error_logs                 FOR sdt_owner.error_logs;
 CREATE SYNONYM error_messages             FOR sdt_owner.error_messages;
 CREATE SYNONYM global_parameters          FOR sdt_owner.global_parameters;
 CREATE SYNONYM individual_requests        FOR sdt_owner.individual_requests;
+CREATE SYNONYM message_logs               FOR sdt_owner.message_logs
 CREATE SYNONYM request_types              FOR sdt_owner.request_types;
 CREATE SYNONYM request_routings           FOR sdt_owner.requst_routings;
 CREATE SYNONYM target_applications        FOR sdt_owner.target_applications;
