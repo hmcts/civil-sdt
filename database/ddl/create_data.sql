19941de4c55415d13864105f9506710865327814
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-10 17:15:55 +0000

    System Task #3266 Added mandatory field to request_types
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16482 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_data.sql
+++ database/ddl/create_data.sql
@@ -1,15 +1,15 @@
 alter session set current_schema=sdt_user;
 
-INSERT INTO request_types (request_type_id,request_type_name)
-VALUES (1,'NEW CLAIM');
-INSERT INTO request_types (request_type_id,request_type_name)
-VALUES (2,'JUDGEMENT');
-INSERT INTO request_types (request_type_id,request_type_name)
-VALUES (3,'WARRANT');
-INSERT INTO request_types (request_type_id,request_type_name)
-VALUES (4,'JUDGEMENT FORTHWITH');
-INSERT INTO request_types (request_type_id,request_type_name)
-VALUES (5,'PAID/WRITTEN OFF/DISCONTINUED/WITHDRAWN REQUESTS');
+INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
+VALUES (1,'NEW CLAIM','A');
+INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
+VALUES (2,'JUDGEMENT','A');
+INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
+VALUES (3,'WARRANT','A');
+INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
+VALUES (4,'JUDGEMENT FORTHWITH','A');
+INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
+VALUES (5,'PAID/WRITTEN OFF/DISCONTINUED/WITHDRAWN REQUESTS','A');
 
 
 
