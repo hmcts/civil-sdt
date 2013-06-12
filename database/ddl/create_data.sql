66841c520a71785964cad583c53ecaf4779ab5f0
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-06-12 12:11:17 +0000

    System Task #3266 Added standing data for global_parameters
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16500 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_data.sql
+++ database/ddl/create_data.sql
@@ -1,5 +1,26 @@
 alter session set current_schema=sdt_user;
 
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+vALUES ( 1,'REQUEST_RETRY_COUNT',10
+,'Number of retry attempts made to transmit an individual request to a third party');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+vALUES ( 2,'BULK_REQUEST_RETENTION_PERIOD',90
+,'Duration in days, to retain data in the tables subject to a prescribed purge');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+vALUES ( 3,'MCOL_ACKNOWLEDGEMENT_TIMEOUT',30
+,'Period in seconds, to wait for an acknowledgement of receipt from MCOL');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+vALUES ( 4,'MAX_CONCURRENT_SDT_REQUESTS',10
+,'Used to determine the maximum number of SDT requests that can be forwarded for processing at any point in time');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+vALUES ( 5,'MONITORING_INTERVAL',60
+,'Time in seconds between ...');
+
+
 INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
 VALUES (1,'NEW CLAIM','A');
 INSERT INTO request_types (request_type_id,request_type_name,request_type_status)
