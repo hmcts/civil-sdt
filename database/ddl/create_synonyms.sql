4435c002055af48f40ada50eb008072637ab9d70
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-08-09 14:34:35 +0000

    Engineering Task #3315 (3.75hrs) FS 0.5 changes
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16817 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_synonyms.sql
+++ database/ddl/create_synonyms.sql
@@ -10,5 +10,6 @@ CREATE SYNONYM individual_requests        FOR sdt_owner.individual_requests;
 CREATE SYNONYM message_logs               FOR sdt_owner.message_logs;
 CREATE SYNONYM request_types              FOR sdt_owner.request_types;
 CREATE SYNONYM request_routings           FOR sdt_owner.requst_routings;
+CREATE SYNONYM service_requests           FOR sdt_owner.service_requests;
 CREATE SYNONYM target_applications        FOR sdt_owner.target_applications;
 
