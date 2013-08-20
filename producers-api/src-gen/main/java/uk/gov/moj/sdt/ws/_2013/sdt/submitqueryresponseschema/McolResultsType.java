ac46f943fe2aafdba4de10732aff31f65f4cfdf8
agarwals <agarwals@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-08-20 15:07:13 +0000

    Engineering Task #3374 (3 hours): The generic XSD for Submit Query Response contained the contents of non-generic XSD and vice-versa. This has now been fixed.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@16891 e0420d7e-bc29-8447-b031-85aca2f896c5

--- producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/submitqueryresponseschema/McolResultsType.java
+++ producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/submitqueryresponseschema/McolResultsType.java
@@ -1,12 +1,11 @@
 
 package uk.gov.moj.sdt.ws._2013.sdt.submitqueryresponseschema;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlType;
-import uk.gov.moj.sdt.ws._2013.mcol.queryschema.McolDefenceDetailType;
+import org.w3c.dom.Element;
 
 
 /**
@@ -19,7 +18,7 @@ import uk.gov.moj.sdt.ws._2013.mcol.queryschema.McolDefenceDetailType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;choice>
- *         &lt;element name="mcolDefenceDetail" type="{http://ws.sdt.moj.gov.uk/2013/mcol/QuerySchema}mcolDefenceDetailType" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;any processContents='lax'/>
  *       &lt;/choice>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -30,39 +29,37 @@ import uk.gov.moj.sdt.ws._2013.mcol.queryschema.McolDefenceDetailType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "mcolResultsType", propOrder = {
-    "mcolDefenceDetail"
+    "any"
 })
 public class McolResultsType {
 
-    protected List<McolDefenceDetailType> mcolDefenceDetail;
+    @XmlAnyElement(lax = true)
+    protected Object any;
 
     /**
-     * Gets the value of the mcolDefenceDetail property.
+     * Gets the value of the any property.
      * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the mcolDefenceDetail property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getMcolDefenceDetail().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link McolDefenceDetailType }
-     * 
-     * 
+     * @return
+     *     possible object is
+     *     {@link Element }
+     *     {@link Object }
+     *     
      */
-    public List<McolDefenceDetailType> getMcolDefenceDetail() {
-        if (mcolDefenceDetail == null) {
-            mcolDefenceDetail = new ArrayList<McolDefenceDetailType>();
-        }
-        return this.mcolDefenceDetail;
+    public Object getAny() {
+        return any;
+    }
+
+    /**
+     * Sets the value of the any property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Element }
+     *     {@link Object }
+     *     
+     */
+    public void setAny(Object value) {
+        this.any = value;
     }
 
 }
