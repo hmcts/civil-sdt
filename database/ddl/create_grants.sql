75d3fd8dd35227a5fc77e83c45e76d47a4dc7ee8
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-09-17 17:22:07 +0000

    Engineering Task #3315 (0.5)
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@17065 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_grants.sql
+++ database/ddl/create_grants.sql
@@ -8,8 +8,8 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON error_logs                 TO sdt_user;
 GRANT SELECT,INSERT,UPDATE,DELETE ON error_messages             TO sdt_user;
 GRANT SELECT,INSERT,UPDATE,DELETE ON global_parameters          TO sdt_user;
 GRANT SELECT,INSERT,UPDATE,DELETE ON individual_requests        TO sdt_user;
-GRANT SELECT,INSERT,UPDATE,DELETE ON message_logs               TO sdt_user;
-GRANT SELECT,INSERT,UPDATE,DELETE ON request_types              TO sdt_user; 
-GRANT SELECT,INSERT,UPDATE,DELETE ON request_routings           TO sdt_user; 
+GRANT SELECT,INSERT,UPDATE,DELETE ON service_types              TO sdt_user; 
+GRANT SELECT,INSERT,UPDATE,DELETE ON service_routings           TO sdt_user; 
+GRANT SELECT,INSERT,UPDATE,DELETE ON service_requests           TO sdt_user; 
 GRANT SELECT,INSERT,UPDATE,DELETE ON target_applications        TO sdt_user;
 
