412ff16b82d90b1f0f77db9a7b2b36f4a5bb0a86
agarwals <agarwals@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-09-20 09:53:20 +0000

    Engineering Task #3315 (1 hours): Adjusted data creation script to setup global parameters as specified in SDT FS v1.0
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@17090 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_data.sql
+++ database/ddl/create_data.sql
@@ -1,30 +1,38 @@
 alter session set current_schema=sdt_user;
 
 INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
-vALUES ( 1,'REQUEST_RETRY_COUNT',10
-,'Number of retry attempts made to transmit an individual request to a third party');
-
-INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
-vALUES ( 2,'BULK_REQUEST_RETENTION_PERIOD',90
+VALUES ( 1,'DATA_RETENTION_PERIOD',90
 ,'Duration in days, to retain data in the tables subject to a prescribed purge');
 
 INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
-vALUES ( 3,'MCOL_ACKNOWLEDGEMENT_TIMEOUT',30
+VALUES ( 2,'MCOL_ACKNOWLEDGEMENT_TIMEOUT',15
 ,'Period in seconds, to wait for an acknowledgement of receipt from MCOL');
 
 INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
-vALUES ( 4,'MAX_CONCURRENT_SDT_REQUESTS',10
-,'Used to determine the maximum number of SDT requests that can be forwarded for processing at any point in time');
+VALUES ( 3,'MAX_FORWARDING_ATTEMPTS',3
+,'Number of forwarding attempts made to transmit an individual request to target application');
 
 INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
-vALUES ( 5,'MONITORING_INTERVAL',60
-,'Time in seconds between ...');
+VALUES ( 4,'MCOL_MAX_CONCURRENT_INDV_REQ',5
+,'Used by the SDT Service to determine the maximum number of concurrent Individual Requests that can be forwarded to MCOL');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+VALUES ( 5,'MCOL_INDV_REQ_DELAY',10
+,'Used by the SDT Service to determine the time delay before processing the next Individual Request that can be forwarded to MCOL');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+VALUES ( 6,'MCOL_MAX_CONCURRENT_QUERY_REQ',5
+,'Used by the SDT Service to determine the maximum number of concurrent Submit Query Requests that can be forwarded to MCOL');
+
+INSERT INTO global_parameters ( global_parameter_id,parameter_name,parameter_value,parameter_description)
+VALUES ( 7,'CONTACT_DETAILS','tbc'
+,'Used by SDT to hold the current contact details for outgoing SDT application messages to Bulk Customer System');
 
 
-INSERT INTO service_types (service_type_id,service_type_name,service_type_status)
-VALUES (1,'INDIVIDUAL REQUEST','A');
-INSERT INTO service_types (service_type_id,service_type_name,service_type_status)
-VALUES (2,'SUBMIT QUERY','A');
+INSERT INTO service_types (service_type_id,service_type_name,service_type_status, service_type_description)
+VALUES (1,'SUBMIT_INDIVIDUAL','A', 'Submit individual request web service');
+INSERT INTO service_types (service_type_id,service_type_name,service_type_status, service_type_description)
+VALUES (2,'SUBMIT_QUERY','A', 'Submit query web service');
 
 
 INSERT INTO target_applications ( target_application_id,target_application_code,target_application_name) 
@@ -37,9 +45,9 @@ INSERT INTO target_applications ( target_application_id,target_application_code,
 VALUES (4,'C_PC','pcol commissioning service');
 
 INSERT INTO service_routings (service_type_id,target_application_id,web_service_endpoint)
-VALUES (1,1,'http://localhost:8888/mcol');
+VALUES (1,1,'http://localhost:8888/mcol-webservices/service');
 INSERT INTO service_routings (service_type_id,target_application_id,web_service_endpoint)
-VALUES (2,1,'http://localhost:8888/pcol');
+VALUES (2,1,'http://localhost:8888/mcol-webservices/service');
 
 
 COMMIT;
