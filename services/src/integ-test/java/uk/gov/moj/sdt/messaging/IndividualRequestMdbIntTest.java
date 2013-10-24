15948d018c2b2cf2d00d806cb0e97acbaf73a9fd
agarwals <agarwals@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-10-24 16:18:15 +0000

    Engineering Task #3460 (0.5 hours): Fixed broken tests.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@17510 e0420d7e-bc29-8447-b031-85aca2f896c5

--- services/src/integ-test/java/uk/gov/moj/sdt/messaging/IndividualRequestMdbIntTest.java
+++ services/src/integ-test/java/uk/gov/moj/sdt/messaging/IndividualRequestMdbIntTest.java
@@ -68,12 +68,12 @@ import uk.gov.moj.sdt.utils.Utilities;
  */
 @RunWith (SpringJUnit4ClassRunner.class)
 @ContextConfiguration (locations = {"classpath*:**/applicationContext.xml",
-        "/uk/gov/moj/sdt/consumers/spring.context.test.xml", "/uk/gov/moj/sdt/dao/spring.context.xml",
+        "/uk/gov/moj/sdt/consumers/spring.context.integ.test.xml", "/uk/gov/moj/sdt/dao/spring.context.xml",
         "/uk/gov/moj/sdt/messaging/spring.hibernate.test.xml", "/uk/gov/moj/sdt/messaging/spring.context.test.xml",
         "/uk/gov/moj/sdt/cache/spring.context.xml", "/uk/gov/moj/sdt/dao/spring*.xml",
         "/uk/gov/moj/sdt/services/spring.context.xml", "/uk/gov/moj/sdt/utils/spring.context.xml",
         "/uk/gov/moj/sdt/utils/transaction/synchronizer/spring.context.xml",
-        "/uk/gov/moj/sdt/transformers/spring.context.xml"})
+        "/uk/gov/moj/sdt/transformers/spring.context.xml", "classpath*:/**/spring*.xml"})
 public class IndividualRequestMdbIntTest extends AbstractTransactionalJUnit4SpringContextTests
 {
     /**
--- services/src/integ-test/java/uk/gov/moj/sdt/utils/SdtBulkReferenceGeneratorIntTest.java
+++ services/src/integ-test/java/uk/gov/moj/sdt/utils/SdtBulkReferenceGeneratorIntTest.java
@@ -54,7 +54,7 @@ import uk.gov.moj.sdt.utils.api.ISdtBulkReferenceGenerator;
  */
 @RunWith (SpringJUnit4ClassRunner.class)
 @ContextConfiguration (locations = {"classpath*:**/applicationContext.xml", "/uk/gov/moj/sdt/dao/spring.context.xml",
-        "/uk/gov/moj/sdt/consumers/spring.context.test.xml", "/uk/gov/moj/sdt/transformers/spring.context.xml",
+        "/uk/gov/moj/sdt/consumers/spring.context.integ.test.xml", "/uk/gov/moj/sdt/transformers/spring.context.xml",
         "classpath*:/**/spring*.xml", "/uk/gov/moj/sdt/dao/spring*.xml", "/uk/gov/moj/sdt/utils/spring*.xml",
         "/uk/gov/moj/sdt/utils/transaction/synchronizer/spring*.xml"})
 @Transactional
