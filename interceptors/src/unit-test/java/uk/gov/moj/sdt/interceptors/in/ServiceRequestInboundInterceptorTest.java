fd437bcea65ded10374fae12ac4c4f41806a1d7b
compstonr <compstonr@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-11-05 16:44:43 +0000

    Engineering task #3303: Fix test by setting up raw xml as if XmlInboundInterceptor had run.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@17589 e0420d7e-bc29-8447-b031-85aca2f896c5

--- interceptors/src/unit-test/java/uk/gov/moj/sdt/interceptors/in/ServiceRequestInboundInterceptorTest.java
+++ interceptors/src/unit-test/java/uk/gov/moj/sdt/interceptors/in/ServiceRequestInboundInterceptorTest.java
@@ -30,27 +30,35 @@
  * $LastChangedBy: $ */
 package uk.gov.moj.sdt.interceptors.in;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.message.ExchangeImpl;
 import org.apache.cxf.message.MessageImpl;
 import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
 
 import uk.gov.moj.sdt.dao.GenericDao;
 import uk.gov.moj.sdt.domain.ServiceRequest;
+import uk.gov.moj.sdt.utils.SdtContext;
 import uk.gov.moj.sdt.utils.SdtUnitTestBase;
+
 /**
  * Test class.
+ * 
  * @author d195274
- *
+ * 
  */
 public class ServiceRequestInboundInterceptorTest extends SdtUnitTestBase
 {
 
     /**
      * Constructor.
+     * 
      * @param testName ronseal
      */
     public ServiceRequestInboundInterceptorTest (final String testName)
@@ -65,16 +73,14 @@ public class ServiceRequestInboundInterceptorTest extends SdtUnitTestBase
      *            possible superfluous object.
      * @return the mocked dao.
      */
-    private GenericDao getMockedGenericDao(final ServiceRequest serviceRequest) {
-        final GenericDao mockServiceRequestDao = EasyMock
-                .createNiceMock(GenericDao.class);
-        mockServiceRequestDao.persist(serviceRequest);
-        EasyMock.expectLastCall();
-        EasyMock.replay(mockServiceRequestDao);
+    private GenericDao getMockedGenericDao (final ServiceRequest serviceRequest)
+    {
+        final GenericDao mockServiceRequestDao = EasyMock.createNiceMock (GenericDao.class);
+        mockServiceRequestDao.persist (serviceRequest);
+        EasyMock.expectLastCall ();
+        EasyMock.replay (mockServiceRequestDao);
         return mockServiceRequestDao;
     }
-
-    
 
     /**
      * Builds a dummy soap message.
@@ -83,12 +89,13 @@ public class ServiceRequestInboundInterceptorTest extends SdtUnitTestBase
      *            the path to the xml file containing the soap message.
      * @return a soap message
      */
-    private SoapMessage getDummySoapMessage(final String resourcePath) {
-        final SoapMessage soapMessage = new SoapMessage(new MessageImpl());
-        soapMessage.setExchange(new ExchangeImpl());
-        soapMessage.setContent(InputStream.class,
-                ServiceRequestInboundInterceptorTest.class
-                        .getResourceAsStream(resourcePath));
+    private SoapMessage getDummySoapMessage (final String resourcePath)
+    {
+        //
+        final SoapMessage soapMessage = new SoapMessage (new MessageImpl ());
+        soapMessage.setExchange (new ExchangeImpl ());
+        soapMessage.setContent (InputStream.class, ServiceRequestInboundInterceptorTest.class
+                .getResourceAsStream (resourcePath));
         return soapMessage;
     }
 
@@ -96,56 +103,112 @@ public class ServiceRequestInboundInterceptorTest extends SdtUnitTestBase
      * Test that the process correctly works via mocked out extensions.
      */
     @Test
-    public void testHandleMessage() {
-        try {
-            
-            final ServiceRequestInboundInterceptor sRII = new ServiceRequestInboundInterceptor();
-            sRII.setServiceRequestDao(getMockedGenericDao(new ServiceRequest()));
-            sRII.handleMessage (getDummySoapMessage ("inRequest.xml"));
-//            final IServiceRequest serviceRequest = sRII..getServiceRequest();
-//            final Long iDField = (Long) getAccesibleField(ServiceRequest.class, "id",
-//                    Long.class, serviceRequest);
-//            assertNull ("id should be null",iDField);
-//            final LocalDateTime requestDateTimeField = (LocalDateTime) getAccesibleField(
-//                    ServiceRequest.class, "requestDateTime", LocalDateTime.class,
-//                    serviceRequest);
-//            assertTrue("requestDateTime should not be null",
-//                    null != requestDateTimeField);
-//            final LocalDateTime responseDateTimeField = (LocalDateTime) getAccesibleField(
-//                    ServiceRequest.class, "responseDateTime", LocalDateTime.class,
-//                    serviceRequest);
-//            assertTrue("responseDateTime should be null",
-//                    null == responseDateTimeField);
-//            final String customerIdField = (String) getAccesibleField(
-//                    ServiceRequest.class, "bulkCustomerId", String.class,
-//                    serviceRequest);
-//            assertTrue("bulkCustomerId should be 1234",
-//                    "1234".equals(customerIdField));
-//            final String sdtRequestIdField = (String) getAccesibleField(
-//                    ServiceRequest.class, "bulkReference", String.class,
-//                    serviceRequest);
-//            assertTrue("bulkReference should be null",
-//                    null == sdtRequestIdField);
-//            final String requestTypeField = (String) getAccesibleField(
-//                    ServiceRequest.class, "requestType", String.class,
-//                    serviceRequest);
-//            assertTrue("requestType should be Claim",
-//                    "Claim".equals(requestTypeField));
-//            final String requestPayloadField = (String) getAccesibleField(
-//                    ServiceRequest.class, "requestPayload", String.class,
-//                    serviceRequest);
-//            assertTrue("requestPayload should not be null",
-//                    null != requestPayloadField);
-//            final String responsePayloadField = (String) getAccesibleField(
-//                    ServiceRequest.class, "responsePayload", String.class,
-//                    serviceRequest);
-//            assertTrue("responsePayload should be null",
-//                    null == responsePayloadField);
-//            
-            
-        } catch (final SecurityException e) {
+    public void testHandleMessage ()
+    {
+        try
+        {
+            // Create the service request inbound interceptor.
+            final ServiceRequestInboundInterceptor sRII = new ServiceRequestInboundInterceptor ();
+
+            // Inject dummy service request into interceptor.
+            sRII.setServiceRequestDao (getMockedGenericDao (new ServiceRequest ()));
+
+            // Setup the raw XML as if the XmlInboundInterceptor had run.
+            final String xml =
+                    this.convertStreamToString (ServiceRequestInboundInterceptorTest.class
+                            .getResourceAsStream ("inRequest.xml"));
+            SdtContext.getContext ().setRawInXml (xml);
+
+            // Call the code to be tested.
+            sRII.handleMessage (new SoapMessage (new MessageImpl ()));
+            // final IServiceRequest serviceRequest = sRII..getServiceRequest();
+            // final Long iDField = (Long) getAccesibleField(ServiceRequest.class, "id",
+            // Long.class, serviceRequest);
+            // assertNull ("id should be null",iDField);
+            // final LocalDateTime requestDateTimeField = (LocalDateTime) getAccesibleField(
+            // ServiceRequest.class, "requestDateTime", LocalDateTime.class,
+            // serviceRequest);
+            // assertTrue("requestDateTime should not be null",
+            // null != requestDateTimeField);
+            // final LocalDateTime responseDateTimeField = (LocalDateTime) getAccesibleField(
+            // ServiceRequest.class, "responseDateTime", LocalDateTime.class,
+            // serviceRequest);
+            // assertTrue("responseDateTime should be null",
+            // null == responseDateTimeField);
+            // final String customerIdField = (String) getAccesibleField(
+            // ServiceRequest.class, "bulkCustomerId", String.class,
+            // serviceRequest);
+            // assertTrue("bulkCustomerId should be 1234",
+            // "1234".equals(customerIdField));
+            // final String sdtRequestIdField = (String) getAccesibleField(
+            // ServiceRequest.class, "bulkReference", String.class,
+            // serviceRequest);
+            // assertTrue("bulkReference should be null",
+            // null == sdtRequestIdField);
+            // final String requestTypeField = (String) getAccesibleField(
+            // ServiceRequest.class, "requestType", String.class,
+            // serviceRequest);
+            // assertTrue("requestType should be Claim",
+            // "Claim".equals(requestTypeField));
+            // final String requestPayloadField = (String) getAccesibleField(
+            // ServiceRequest.class, "requestPayload", String.class,
+            // serviceRequest);
+            // assertTrue("requestPayload should not be null",
+            // null != requestPayloadField);
+            // final String responsePayloadField = (String) getAccesibleField(
+            // ServiceRequest.class, "responsePayload", String.class,
+            // serviceRequest);
+            // assertTrue("responsePayload should be null",
+            // null == responsePayloadField);
+            //
+
+        }
+        catch (final SecurityException e)
+        {
             // TODO Auto-generated catch block
-            e.printStackTrace();
-        } 
+            e.printStackTrace ();
+        }
+    }
+
+    /**
+     * Utility method to get contents from input stream.
+     * 
+     * @param is input stream from which to extract contents.
+     * @return contents of input stream.
+     */
+    private String convertStreamToString (final InputStream is)
+    {
+        /* To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String. */
+        final StringBuilder sb = new StringBuilder ();
+        String line;
+
+        try
+        {
+            final BufferedReader reader = new BufferedReader (new InputStreamReader (is, "UTF-8"));
+            while ((line = reader.readLine ()) != null)
+            {
+                sb.append (line).append ("\n");
+            }
+        }
+        catch (final IOException e)
+        {
+            Assert.fail (e.getLocalizedMessage ());
+        }
+        finally
+        {
+            try
+            {
+                is.close ();
+            }
+            catch (final IOException e)
+            {
+                // Do nothing.
+            }
+        }
+
+        return sb.toString ();
     }
 }
