93760851e0b64dfaf592c8790cd716374216388a
nigamam <nigamam@e0420d7e-bc29-8447-b031-85aca2f896c5>
2013-11-07 15:37:49 +0000

    Engineering Task #3454: Correction to throw outage exception when server unavailable.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@17605 e0420d7e-bc29-8447-b031-85aca2f896c5

--- consumers/src/main/java/uk/gov/moj/sdt/consumers/AbstractWsConsumer.java
+++ consumers/src/main/java/uk/gov/moj/sdt/consumers/AbstractWsConsumer.java
@@ -45,6 +45,7 @@ import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 
+import uk.gov.moj.sdt.consumers.exception.OutageException;
 import uk.gov.moj.sdt.consumers.exception.SoapFaultException;
 import uk.gov.moj.sdt.consumers.exception.TimeoutException;
 import uk.gov.moj.sdt.utils.mbeans.SdtMetricsMBean;
@@ -159,13 +160,13 @@ public abstract class AbstractWsConsumer
      *            when the server is down.
      * @param wsException - the WebServiceException caught by the client submit call.
      * @param errorReferenceContext - optional parameter indicating the unique reference to identify the request.
-     * @throws WebServiceException - if there is a connection failure, the web service exception may be re-thrown.
+     * @throws OutageException - if there is a connection failure, the web service exception may be re-thrown.
      * @throws TimeoutException - if there is a read timeout issue, throw the time out exception.
      * @throws SoapFaultException - if the soap request resulted in an soap fault.
      */
     protected void handleClientErrors (final boolean rethrowOnFailureToConnect, final WebServiceException wsException,
                                        final String errorReferenceContext)
-        throws WebServiceException, TimeoutException, SoapFaultException
+        throws OutageException, TimeoutException, SoapFaultException
     {
         // If the target application is unavailable continue trying to send message indefinitely.
         if ((wsException.getCause () instanceof ConnectException) && rethrowOnFailureToConnect)
@@ -177,7 +178,7 @@ public abstract class AbstractWsConsumer
             // of queue count done earlier.
             SdtMetricsMBean.getSdtMetrics ().upRequestQueueLength ();
 
-            throw wsException;
+            throw new OutageException ("OUTAGE_ERROR", wsException.getMessage ());
         }
         // If the target application is unavailable continue trying to send message indefinitely.
         else if (wsException.getCause () instanceof ConnectException)
