eebeec1ba6777aa444b02ef4c52747c1da6ce1a9
agarwals <agarwals@e0420d7e-bc29-8447-b031-85aca2f896c5>
2014-03-05 15:10:57 +0000

    Engineering Task #3656: Modified XSD to enforce that sotSignatureType#flag is always populated and only accepts 'true' as valid value. Empty tag will be rejected with XSD error.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@18436 e0420d7e-bc29-8447-b031-85aca2f896c5

--- producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/baseschema/SotSignatureType.java
+++ producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/baseschema/SotSignatureType.java
@@ -17,7 +17,13 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="flag" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
+ *         &lt;element name="flag">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}boolean">
+ *               &lt;pattern value="true"/>
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
  *         &lt;element name="name">
  *           &lt;simpleType>
  *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
