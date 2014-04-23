deacaa82fc06552f174ae29368c5ff191deff1f8
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2014-04-23 17:17:41 +0000

    Engineering Task #3649 ( 0.25 ) : Amending 3 fks to on delete cascade. Delete from service_requests, bulk_submissions and individual_requests with extreme care !
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@18855 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_foreign_keys.sql
+++ database/ddl/create_foreign_keys.sql
@@ -32,6 +32,7 @@ ALTER TABLE bulk_submissions
 ADD CONSTRAINT bs_service_request_id_fk
 FOREIGN KEY (service_request_id)
 REFERENCES service_requests(service_request_id)
+ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
@@ -39,6 +40,7 @@ ALTER TABLE error_logs
 ADD CONSTRAINT el_individual_request_fk
 FOREIGN KEY (individual_request_id)
 REFERENCES individual_requests(individual_request_id)
+ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
@@ -47,6 +49,7 @@ ALTER TABLE individual_requests
 ADD CONSTRAINT ir_bulk_submission_fk
 FOREIGN KEY (bulk_submission_id)
 REFERENCES bulk_submissions(bulk_submission_id)
+ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
