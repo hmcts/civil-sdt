a5e1435586c28e806fd7b1d1d10a3bb61fe90351
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2014-05-14 09:51:53 +0000

    Engineering Task #3649 ( 1.5 ) : reverting fk changes to remove cascade
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@18932 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_foreign_keys.sql
+++ database/ddl/create_foreign_keys.sql
@@ -32,7 +32,6 @@ ALTER TABLE bulk_submissions
 ADD CONSTRAINT bs_service_request_id_fk
 FOREIGN KEY (service_request_id)
 REFERENCES service_requests(service_request_id)
-ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
@@ -40,7 +39,6 @@ ALTER TABLE error_logs
 ADD CONSTRAINT el_individual_request_fk
 FOREIGN KEY (individual_request_id)
 REFERENCES individual_requests(individual_request_id)
-ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
@@ -49,7 +47,6 @@ ALTER TABLE individual_requests
 ADD CONSTRAINT ir_bulk_submission_fk
 FOREIGN KEY (bulk_submission_id)
 REFERENCES bulk_submissions(bulk_submission_id)
-ON DELETE CASCADE
 NOT DEFERRABLE INITIALLY IMMEDIATE
 ;
 
