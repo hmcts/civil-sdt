3544b8b521ebef0e2a370d80a5717bc09179ef49
jonesb <jonesb@e0420d7e-bc29-8447-b031-85aca2f896c5>
2014-05-20 12:56:58 +0000

    Engineering Task #3649 ( 3.75) : Adding missing indexes corresponding to columns referencing foreign keys
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@18960 e0420d7e-bc29-8447-b031-85aca2f896c5

--- database/ddl/create_indexes.sql
+++ database/ddl/create_indexes.sql
@@ -1,23 +1,18 @@
 alter session set current_schema=sdt_owner;
 
-
-DEFINE bs_sdt_bulk_reference_i       = 'TABLESPACE users'
 DEFINE bca_bulk_customer_i           = 'TABLESPACE users'
 DEFINE bca_target_application_i      = 'TABLESPACE users'
+DEFINE bs_sdt_bulk_reference_i       = 'TABLESPACE users'
+DEFINE bs_target_application_id_i    = 'TABLESPACE users'
+DEFINE bs_bulk_customer_id_i         = 'TABLESPACE users'
+DEFINE bs_service_request_id_i       = 'TABLESPACE users'
+DEFINE el_individual_request_id_i    = 'TABLESPACE users'
 DEFINE ir_bulk_reference_i           = 'TABLESPACE users'
+DEFINE ir_bulk_submission_id_i       = 'TABLESPACE users'
 DEFINE sr_service_type_i             = 'TABLESPACE users'
 DEFINE sr_target_application_i       = 'TABLESPACE users'
 DEFINE st_service_type_name_i        = 'TABLESPACE users'
 DEFINE ta_target_application_name_i  = 'TABLESPACE users'
-
---
--- bulk_submissions
---
-
-CREATE INDEX bs_sdt_bulk_reference_i
-ON bulk_submissions ( sdt_bulk_reference )
-&bs_sdt_bulk_reference_i
-;
 
 --
 -- bulk_customer_applications
@@ -34,6 +29,35 @@ ON bulk_customer_applications ( target_application_id)
 ;
 
 --
+-- bulk_submissions
+--
+
+CREATE INDEX bs_sdt_bulk_reference_i
+ON bulk_submissions ( sdt_bulk_reference )
+&bs_sdt_bulk_reference_i
+;
+
+CREATE INDEX bs_target_application_id_i
+ON bulk_submissions ( target_application_id )
+&bs_target_application_id_i;
+
+CREATE INDEX bs_bulk_customer_id_i
+ON bulk_submissions ( bulk_customer_id )
+&bs_bulk_customer_id_i;
+
+CREATE INDEX bs_service_request_id_i
+ON bulk_submissions( service_request_id )
+&bs_service_request_id_i;
+
+--
+-- error_logs
+--
+
+CREATE INDEX el_individual_request_id_i
+ON error_logs(individual_request_id)
+&el_individual_request_id_i;
+
+--
 --individual_requests
 --
 
@@ -42,10 +66,13 @@ ON individual_requests ( sdt_bulk_reference )
 &ir_bulk_reference_i
 ;
 
+CREATE INDEX ir_bulk_submission_id_i
+ON individual_requests ( bulk_submission_id )
+&ir_bulk_submission_id_i;
+
 --
 -- service_routings
 --
-
 CREATE INDEX sr_service_type_i
 ON service_routings (service_type_id)
 &sr_service_type_i
@@ -64,7 +91,6 @@ ON service_types ( service_type_name )
 &st_service_type_name_i
 ;
 
-
 -- 
 -- target_applications
 --
@@ -72,3 +98,5 @@ CREATE INDEX ta_target_application_name
 ON target_applications ( target_application_name)
 &ta_target_application_name_i
 ;
+
+
