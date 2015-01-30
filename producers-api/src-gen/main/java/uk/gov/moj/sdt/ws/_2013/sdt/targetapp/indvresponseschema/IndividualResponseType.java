36e87df370e24f6bd4adfe4b0cfcbc0b8e7a2e37
ghulema <ghulema@e0420d7e-bc29-8447-b031-85aca2f896c5>
2015-01-30 17:10:17 +0000

    Engineering Task #4064: section 38 compliancy flag added Code regenerated
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@19824 e0420d7e-bc29-8447-b031-85aca2f896c5

--- producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/targetapp/indvresponseschema/IndividualResponseType.java
+++ producers-api/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/sdt/targetapp/indvresponseschema/IndividualResponseType.java
@@ -161,8 +161,8 @@ public class IndividualResponseType {
          * 
          * @return
          *     possible object is
-         *     {@link Element }
          *     {@link Object }
+         *     {@link Element }
          *     
          */
         public Object getAny() {
@@ -174,8 +174,8 @@ public class IndividualResponseType {
          * 
          * @param value
          *     allowed object is
-         *     {@link Element }
          *     {@link Object }
+         *     {@link Element }
          *     
          */
         public void setAny(Object value) {
