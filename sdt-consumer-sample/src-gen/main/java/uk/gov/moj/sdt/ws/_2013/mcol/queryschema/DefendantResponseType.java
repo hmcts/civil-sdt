b756d5239b123f58a7814fbf7e4ca37fd6356a1f
compstonr <compstonr@e0420d7e-bc29-8447-b031-85aca2f896c5>
2016-02-19 16:56:29 +0000

    Engineering Task #4227: Add new type in Base.xsd for defence (8000 bytes). Use this type in query XSD. Do the same in SDT consumer sample. Add defence field in various forms to test. Exercise XSD validation on 8000 byte defence field. Add defence to SoapUi scripts. Update commissioning system to return defence in results. Add missing eventCreatedDateOnMcol field (someone forgot this in previous change). Update Gateway WSDL for publication to gateway.
    git-svn-id: http://mcoltrac/svn/mcol-app/sdt/trunk@20500 e0420d7e-bc29-8447-b031-85aca2f896c5

--- sdt-consumer-sample/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/mcol/queryschema/DefendantResponseType.java
+++ sdt-consumer-sample/src-gen/main/java/uk/gov/moj/sdt/ws/_2013/mcol/queryschema/DefendantResponseType.java
@@ -23,8 +23,10 @@ import org.w3._2001.xmlschema.Adapter1;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="filedDate" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="eventCreatedDateOnMcol" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
  *         &lt;element name="raisedOnMcol" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
  *         &lt;element name="responseType" type="{http://ws.sdt.moj.gov.uk/2013/mcol/QuerySchema}responseType"/>
+ *         &lt;element name="defence" type="{http://ws.sdt.moj.gov.uk/2013/sdt/BaseSchema}defenceType" minOccurs="0"/>
  *       &lt;/sequence>
  *       &lt;attribute name="defendantId" use="required" type="{http://ws.sdt.moj.gov.uk/2013/sdt/BaseSchema}defendantIdType" />
  *     &lt;/restriction>
@@ -37,8 +39,10 @@ import org.w3._2001.xmlschema.Adapter1;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "defendantResponseType", propOrder = {
     "filedDate",
+    "eventCreatedDateOnMcol",
     "raisedOnMcol",
-    "responseType"
+    "responseType",
+    "defence"
 })
 public class DefendantResponseType {
 
@@ -46,9 +50,14 @@ public class DefendantResponseType {
     @XmlJavaTypeAdapter(Adapter1 .class)
     @XmlSchemaType(name = "dateTime")
     protected Calendar filedDate;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(Adapter1 .class)
+    @XmlSchemaType(name = "dateTime")
+    protected Calendar eventCreatedDateOnMcol;
     protected boolean raisedOnMcol;
     @XmlElement(required = true)
     protected ResponseType responseType;
+    protected String defence;
     @XmlAttribute(name = "defendantId", required = true)
     protected String defendantId;
 
@@ -74,6 +83,30 @@ public class DefendantResponseType {
      */
     public void setFiledDate(Calendar value) {
         this.filedDate = value;
+    }
+
+    /**
+     * Gets the value of the eventCreatedDateOnMcol property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public Calendar getEventCreatedDateOnMcol() {
+        return eventCreatedDateOnMcol;
+    }
+
+    /**
+     * Sets the value of the eventCreatedDateOnMcol property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEventCreatedDateOnMcol(Calendar value) {
+        this.eventCreatedDateOnMcol = value;
     }
 
     /**
@@ -114,6 +147,30 @@ public class DefendantResponseType {
      */
     public void setResponseType(ResponseType value) {
         this.responseType = value;
+    }
+
+    /**
+     * Gets the value of the defence property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDefence() {
+        return defence;
+    }
+
+    /**
+     * Sets the value of the defence property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDefence(String value) {
+        this.defence = value;
     }
 
     /**
