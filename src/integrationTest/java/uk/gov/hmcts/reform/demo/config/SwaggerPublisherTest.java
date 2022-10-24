96723643257823cb30279f9b8a521a57099c6150
Kiran Yenigala <kiran.yenigala@nihr.ac.uk>
2022-10-24 13:31:48 +0100

    SDT-52: Fix test case failures and CVE issues

--- src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = { "spring.config.location=classpath:application-integration-test.yaml" })
 @AutoConfigureMockMvc
 class SwaggerPublisherTest {
 
