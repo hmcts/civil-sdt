fe51e99ea24fdfc1012817f1aff77a4c75e24d64
markdathorne <mark.dathorne@cgi.com>
2022-11-30 16:50:42 +0000

    SDT-19 dev review changes - add constraints, thus revamp audit tables creation and id retrieval.

--- src/main/resources/db/migration/V0002__purge.sql
+++ src/main/resources/db/migration/V0002__purge.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS sdt_owner.purge_job_audit;
-DROP TABLE IF EXISTS sdt_owner.purge_job_audit_messages;
+DROP TABLE IF EXISTS sdt_owner.purge_job_audit CASCADE;
+DROP TABLE IF EXISTS sdt_owner.purge_job_audit_messages CASCADE;
 
 CREATE TABLE sdt_owner.purge_job_audit
-(purge_job_id SERIAL PRIMARY KEY,
+(purge_job_id NUMERIC NOT NULL,
 job_start_date TIMESTAMP(6),
 job_end_date TIMESTAMP(6),
 retention_date TIMESTAMP(6),
@@ -13,18 +13,31 @@ count_of_service_requests NUMERIC,
 success NUMERIC);
 
 CREATE TABLE sdt_owner.purge_job_audit_messages
-(purge_job_message_id SERIAL PRIMARY KEY,
+(purge_job_message_id NUMERIC NOT NULL,
 purge_job_id NUMERIC,
 created_date TIMESTAMP(6),
 log_message VARCHAR(256));
 
-CREATE OR REPLACE FUNCTION sdt_owner.purge (i_CommitInterval IN INT) RETURNS VOID language plpgsql AS $$
+-- add indexes --
+CREATE INDEX pja_purge_job_id ON sdt_owner.purge_job_audit (purge_job_id);
+CREATE INDEX pjam_purge_job_message_id ON sdt_owner.purge_job_audit_messages (purge_job_message_id);
+
+CREATE UNIQUE INDEX purge_job_audit_pk ON sdt_owner.purge_job_audit (purge_job_id);
+CREATE UNIQUE INDEX purge_job_audit_messages_pk ON sdt_owner.purge_job_audit_messages (purge_job_message_id);
+
+-- add constraints --
+ALTER TABLE sdt_owner.purge_job_audit ADD CONSTRAINT purge_job_audit_pk PRIMARY KEY USING INDEX purge_job_audit_pk;
+ALTER TABLE sdt_owner.purge_job_audit_messages ADD CONSTRAINT purge_job_audit_messages_pk PRIMARY KEY USING INDEX purge_job_audit_messages_pk;
+
+
+CREATE OR REPLACE PROCEDURE sdt_owner.purge (i_CommitInterval IN INT) language plpgsql AS $$
 DECLARE
 DECLARE i_retention_period VARCHAR(32);
 DECLARE t_retention_date TIMESTAMP;
 DECLARE t_job_start_date TIMESTAMP;
 DECLARE t_job_end_date TIMESTAMP;
 DECLARE i_purge_job_id NUMERIC;
+DECLARE i_purge_job_message_id NUMERIC;
 
   -- cursor1 service requests created before given retention date
   c1 CURSOR (n_param DOUBLE PRECISION)
@@ -89,73 +102,96 @@ DECLARE i_purge_job_id NUMERIC;
     INTO t_retention_date;
     RAISE NOTICE 'Retention period date: %', t_retention_date;
 
+	-- get job audit id
+	SELECT case count(*) when 0 then 0 else MAX(purge_job_id) end
+	INTO i_purge_job_id
+	FROM sdt_owner.purge_job_audit;
+
+	i_purge_job_id := i_purge_job_id + 1;
+    RAISE NOTICE 'i_purge_job_id is %', i_purge_job_id;
+
+	-- get job message id
+	SELECT case count(*) when 0 then 0 else MAX(purge_job_message_id) end
+	INTO i_purge_job_message_id
+    FROM sdt_owner.purge_job_audit_messages;
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
+    RAISE NOTICE 'i_purge_job_message_id is %', i_purge_job_message_id;
+
     -- create job audit
 	INSERT INTO sdt_owner.purge_job_audit
-  	( job_start_date, success)
-  	VALUES (t_job_start_date, 0);
-
-    -- get job audit id
-	SELECT max(purge_job_id)
-    INTO i_purge_job_id
-    FROM sdt_owner.purge_job_audit;
+  	(purge_job_id, job_start_date, retention_date, success)
+  	VALUES (i_purge_job_id, t_job_start_date, t_retention_date, 0);
 
     RAISE NOTICE 'Purge retention period is % days, deleting all data prior to %',
                           i_retention_period,
                           t_retention_date;
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), 'Purge retention period is ' || i_retention_period ||
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), 'Purge retention period is ' || i_retention_period ||
 			' days, deleting all data prior to ' || t_retention_date);
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
 
     -- delete error logs
     RAISE NOTICE 'Commencing deletion of error_logs records';
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), 'Commencing deletion of error_logs records');
-    n_iteration := 0;
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), 'Commencing deletion of error_logs records');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
+
+	n_iteration := 0;
     FOR c4_rec IN c4(i_retention_period) LOOP
       DELETE FROM sdt_owner.error_logs
       WHERE error_log_id = c4_rec.error_log_id;
       RAISE NOTICE 'Deleted error_logs : %', c4_rec.error_log_id;
       n_iteration := n_iteration + 1;
-      IF MOD(n_iteration,i_CommitInterval)=0 THEN
-        /* COMMIT; */
-      END IF;
     END LOOP;
+
     RAISE NOTICE '% records deleted from error_logs table.', n_iteration;
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), n_iteration || ' records deleted from error_logs table.');
-    -- update count of deletions - error logs
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), n_iteration || ' records deleted from error_logs table.');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
+
+	-- update count of deletions - error logs
     UPDATE sdt_owner.purge_job_audit
   	SET count_of_error_logs=n_iteration
   	WHERE purge_job_id=i_purge_job_id;
 
     RAISE NOTICE 'Commencing deletion of individual_request records';
+
     n_iteration := 0;
     FOR c3_rec IN c3(i_retention_period) LOOP
       DELETE FROM sdt_owner.individual_requests
       WHERE individual_request_id = c3_rec.individual_request_id;
       RAISE NOTICE 'Deleted individual_request : %', c3_rec.sdt_request_reference;
       n_iteration := n_iteration + 1;
-      IF MOD(n_iteration,i_CommitInterval)=0 THEN
-        /* COMMIT; */
-      END IF;
     END LOOP;
-    /* COMMIT; */
+
     RAISE NOTICE '% records deleted from individual_requests table.', n_iteration;
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), n_iteration || ' records deleted from individual_requests table.');
-    -- update count of deletions - error logs
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), n_iteration || ' records deleted from individual_requests table.');
+	i_purge_job_message_id := i_purge_job_message_id + 1;
+
+	-- update count of deletions - error logs
     UPDATE sdt_owner.purge_job_audit
   	SET count_of_individual_requests=n_iteration
   	WHERE purge_job_id=i_purge_job_id;
 
     RAISE NOTICE 'Commencing deletion of bulk_submissions records';
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), 'Commencing deletion of bulk_submissions records');
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), 'Commencing deletion of bulk_submissions records');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
 
     n_iteration := 0;
     FOR c2_rec IN c2(i_retention_period) LOOP
@@ -163,23 +199,26 @@ DECLARE i_purge_job_id NUMERIC;
       WHERE bulk_submission_id = c2_rec.bulk_submission_id;
       RAISE NOTICE 'Deleted bulk_submissions : %', c2_rec.bulk_submission_id;
       n_iteration := n_iteration + 1;
-      IF MOD(n_iteration,i_CommitInterval)=0 THEN
-        /* COMMIT; */
-      END IF;
     END LOOP;
-    /* COMMIT; */
+
     RAISE NOTICE '% records deleted from bulk_submissions table.', n_iteration;
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), n_iteration || ' records deleted from bulk_submissions table.');
-    UPDATE sdt_owner.purge_job_audit
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), n_iteration || ' records deleted from bulk_submissions table.');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
+
+	UPDATE sdt_owner.purge_job_audit
   	SET count_of_bulk_submissions=n_iteration
   	WHERE purge_job_id=i_purge_job_id;
 
     RAISE NOTICE 'Commencing deletion of service_requests records';
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), 'Commencing deletion of service_requests records');
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), 'Commencing deletion of service_requests records');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
 
     n_iteration := 0;
     FOR c1_rec IN c1(i_retention_period) LOOP
@@ -187,23 +226,19 @@ DECLARE i_purge_job_id NUMERIC;
       WHERE service_request_id = c1_rec.service_request_id;
       RAISE NOTICE 'Deleted service_requests : %', c1_rec.service_request_id;
       n_iteration := n_iteration + 1;
-      IF MOD(n_iteration,i_CommitInterval)=0 THEN
-        /* COMMIT; */
-      END IF;
     END LOOP;
-    /* COMMIT; */
+
     RAISE NOTICE '% records deleted from service_requests table.', n_iteration;
+
     INSERT INTO sdt_owner.purge_job_audit_messages
-    (purge_job_id, created_date,log_message)
-    VALUES (i_purge_job_id, now(), n_iteration || ' records deleted from service_requests table.');
-    UPDATE sdt_owner.purge_job_audit
-  	SET count_of_service_requests=n_iteration
-  	WHERE purge_job_id=i_purge_job_id;
+    (purge_job_message_id, purge_job_id, created_date,log_message)
+    VALUES (i_purge_job_message_id, i_purge_job_id, now(), n_iteration || ' records deleted from service_requests table.');
+
+	i_purge_job_message_id := i_purge_job_message_id + 1;
 
 	UPDATE sdt_owner.purge_job_audit
-  	SET job_end_date=now(), success=1
+  	SET count_of_service_requests=n_iteration,job_end_date=now(), success=1
   	WHERE purge_job_id=i_purge_job_id;
 
   END;
 $$
-
