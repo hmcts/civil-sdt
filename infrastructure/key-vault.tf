b8b09d4dd21ab422ddad3f2bbcf4ddd1a3752f0a
Kiran Yenigala <kiran.yenigala@hmcts.net>
2022-12-15 12:09:19 +0000

    SDT-24: Configure ASB Message queue

---
+++ infrastructure/key-vault.tf
@@ -0,0 +1,58 @@
+module "vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = "${var.product}-${var.env}"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = "dcd_civil"
+  common_tags             = local.tags
+  create_managed_identity = true
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "civil_sdt_service_s2s_key" {
+  name         = "microservicekey-civil-sdt-service"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "civil_sdt_service_s2s_secret" {
+  name         = "civil-sdt-service-s2s-secret"
+  value        = data.azurerm_key_vault_secret.civil_sdt_service_s2s_key.value
+  key_vault_id = module.vault.key_vault_id
+}
+
+data "azurerm_key_vault_secret" "civil_sdt_inbound_adapter_s2s_key" {
+  name         = "microservicekey-civil-sdt-inbound-adapter"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "civil_sdt_inbound_adapter_s2s_secret" {
+  name         = "civil-sdt-inbound-adapter-s2s-secret"
+  value        = data.azurerm_key_vault_secret.civil_sdt_inbound_adapter_s2s_key.value
+  key_vault_id = module.vault.key_vault_id
+}
+
+data "azurerm_key_vault_secret" "api_gw_s2s_key" {
+  name         = "microservicekey-api-gw"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "api_gw_s2s_secret" {
+  name         = "api-gateway-s2s-secret"
+  value        = data.azurerm_key_vault_secret.api_gw_s2s_key.value
+  key_vault_id = module.vault.key_vault_id
+}
+
+output "vaultName" {
+  value = module.vault.key_vault_name
+}
+
+output "vaultUri" {
+  value = module.vault.key_vault_uri
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,8 @@
-variable "product" {}
+variable "product" {
+  type        = string
+  description = "The name of your application"
+  default     = "civil-sdt"
+}
 
 variable "component" {}
 
@@ -14,4 +18,30 @@ variable "deployment_namespace" {}
 
 variable "common_tags" {
   type = "map"
+}
+
+variable "team_contact" {
+  type        = string
+  description = "The name of your Slack channel people can use to contact your team about your infrastructure"
+  default     = "#civil-sdt"
+}
+
+variable "destroy_me" {
+  type        = string
+  description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
+  default     = "No"
+}
+
+variable "sku" {
+  type        = string
+  default     = "Premium"
+  description = "SKU type(Basic, Standard and Premium)"
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
