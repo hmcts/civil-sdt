cc9827e9d799791cb59564be5bc59ce433c874da
markdathorne <mark.dathorne@cgi.com>
2023-01-09 18:01:38 +0000

    SDT-84 dev review - use mockito class annotation only.

--- producers/src/unit-test/java/uk/gov/moj/sdt/producers/sdtws/SdtInternalEndpointPortTypeTest.java
+++ producers/src/unit-test/java/uk/gov/moj/sdt/producers/sdtws/SdtInternalEndpointPortTypeTest.java
@@ -34,7 +34,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.moj.sdt.handlers.api.IWsUpdateItemHandler;
 import uk.gov.moj.sdt.utils.AbstractSdtUnitTestBase;
@@ -76,8 +75,6 @@ class SdtInternalEndpointPortTypeTest extends AbstractSdtUnitTestBase {
     @BeforeEach
     @Override
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-
         portType = new SdtInternalEndpointPortType();
         portType.setUpdateItemHandler(mockUpdateItemHandler);
     }
