8bcd124534c1e2bbd2304fa9dc7c17490716c0dc
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-01-31 22:56:53 +0000

    SDT-90: delete unused class

--- producers/src/unit-test/java/uk/gov/moj/sdt/producers/sdtws/config/ProducersConfig.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.moj.sdt.producers.sdtws.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("uk.gov.moj.sdt")
-public class ProducersConfig {
-}
