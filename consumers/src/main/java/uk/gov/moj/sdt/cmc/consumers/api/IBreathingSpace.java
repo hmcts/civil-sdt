86c40d25721ce059fdebae53b096cb58112fb56d
markdathorne <mark.dathorne@cgi.com>
2023-02-16 13:56:32 +0000

    SDT-99 removed unused import annotation RequestHeader

--- consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/api/IBreathingSpace.java
+++ consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/api/IBreathingSpace.java
@@ -1,6 +1,5 @@
 package uk.gov.moj.sdt.cmc.consumers.api;
 
-import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.moj.sdt.domain.api.IIndividualRequest;
 
 public interface IBreathingSpace {
