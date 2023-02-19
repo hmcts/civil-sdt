2e360c8ed3af7b6316d6edcfdb35c38c795ef890
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-02-19 11:08:54 +0000

    SDT-41: Update Claim status flow

--- consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/converter/XmlToObject.java
+++ consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/converter/XmlToObject.java
@@ -11,9 +11,8 @@ public final class XmlToObject {
 
     public static <T> T  convertXmlToObject(String xml,
                                      Class<T> targetClass) throws IOException {
-        String json = convertXmlToJson(xml);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(json, targetClass);
+        XmlMapper xmlMapper = new XmlMapper();
+        return xmlMapper.readValue(xml, targetClass);
     }
 
     public static String convertXmlToJson(String xml) throws IOException {
