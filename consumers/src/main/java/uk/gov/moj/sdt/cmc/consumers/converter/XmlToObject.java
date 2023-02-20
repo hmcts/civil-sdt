3c7af1ca2e4fa15f2862f49ebe1b3dacc57a76af
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-02-20 12:46:15 +0000

    SDT-41: convert object to xml

--- consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/converter/XmlToObject.java
+++ consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/converter/XmlToObject.java
@@ -1,5 +1,6 @@
 package uk.gov.moj.sdt.cmc.consumers.converter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.IOException;
@@ -19,5 +20,10 @@ public final class XmlToObject {
         XmlMapper xmlMapper = new XmlMapper();
         ObjectMapper jsonMapper = new ObjectMapper();
         return jsonMapper.writeValueAsString(xmlMapper.readTree(xml.getBytes()));
+    }
+
+    public static <T> String convertObjectToXml(T object) throws JsonProcessingException {
+        XmlMapper xmlMapper = new XmlMapper();
+        return xmlMapper.writeValueAsString(object);
     }
 }
