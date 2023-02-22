8ffd568fec6a2902a45b6c44312466aab16bc565
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-02-22 12:55:09 +0000

    SDT-42: Fix compilation issues

--- consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/client/impl/ClaimStatusUpdateService.java
+++ consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/client/impl/ClaimStatusUpdateService.java
@@ -2,19 +2,19 @@ package uk.gov.moj.sdt.cmc.consumers.client.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.moj.sdt.cmc.consumers.api.CMCApi;
 import uk.gov.moj.sdt.cmc.consumers.model.ClaimStatusUpdateRequest;
-import uk.gov.moj.sdt.cmc.consumers.api.CmcApi;
 import uk.gov.moj.sdt.cmc.consumers.api.IClaimStatusUpdate;
 
 @Service("ClaimStatusUpdateService")
 public class ClaimStatusUpdateService implements IClaimStatusUpdate {
 
-    private CmcApi cmcApi;
+    private CMCApi cmcApi;
 
     private ClaimStatusUpdateRequest claimStatusUpdateRequestObj;
 
     @Autowired
-    public ClaimStatusUpdateService(CmcApi cmcApi) {
+    public ClaimStatusUpdateService(CMCApi cmcApi) {
         this.cmcApi = cmcApi;
     }
     @Override
