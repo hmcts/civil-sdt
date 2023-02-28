5601db5975fa3054f313144347be234dfee4c7d5
markdathorne <mark.dathorne@cgi.com>
2023-02-28 16:16:40 +0000

    SDT-99 *WIP* Fix test xml ClaimDefences and reinstate  unit test

--- consumers/src/unit-test/java/uk/gov/moj/sdt/cmc/consumers/client/xml/XmlReaderTest.java
+++ consumers/src/unit-test/java/uk/gov/moj/sdt/cmc/consumers/client/xml/XmlReaderTest.java
@@ -22,23 +22,22 @@ class XmlReaderTest extends BaseXmlTest {
         assertEquals("H0PR0001", claimNumberValue);
     }
 
-// TODO: fix broken test
-//    @Test
-//    void findValuesFromClaimDefencesXmlString() {
-//        String xmlContent = readXmlAsString(CLAIM_DEFENCES);
-//        XmlElementValueReader xmlReader = new XmlElementValueReader();
-//        String sdtCustomerId = xmlReader.getElementValue(xmlContent, "sdtCustomerId");
-//        String targetApplicationId = xmlReader.getElementValue(xmlContent, "targetApplicationId");
-//        String fromDate = xmlReader.getElementValue(xmlContent, "fromDate");
-//        String toDate = xmlReader.getElementValue(xmlContent, "toDate");
-//        assertNotNull(sdtCustomerId);
-//        assertEquals("12345678", sdtCustomerId);
-//        assertNotNull(targetApplicationId);
-//        assertEquals("mcol", targetApplicationId);
-//        assertNotNull(fromDate);
-//        assertEquals("2009-12-01", fromDate);
-//        assertNotNull(toDate);
-//        assertEquals("2009-12-02", toDate);
-//    }
+    @Test
+    void findValuesFromClaimDefencesXmlString() {
+        String xmlContent = readXmlAsString(CLAIM_DEFENCES);
+        XmlElementValueReader xmlReader = new XmlElementValueReader();
+        String sdtCustomerId = xmlReader.getElementValue(xmlContent, "sdtCustomerId");
+        String targetApplicationId = xmlReader.getElementValue(xmlContent, "targetApplicationId");
+        String fromDate = xmlReader.getElementValue(xmlContent, "fromDate");
+        String toDate = xmlReader.getElementValue(xmlContent, "toDate");
+        assertNotNull(sdtCustomerId);
+        assertEquals("12345678", sdtCustomerId);
+        assertNotNull(targetApplicationId);
+        assertEquals("mcol", targetApplicationId);
+        assertNotNull(fromDate);
+        assertEquals("2009-12-01", fromDate);
+        assertNotNull(toDate);
+        assertEquals("2009-12-02", toDate);
+    }
 
 }
