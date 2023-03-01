a6369a9bf6cc709154872e8941cfd4c66172a8ef
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-03-01 14:27:05 +0000

    SDT-42: Addressed issue with validating individual requests

--- utils/src/main/java/uk/gov/moj/sdt/utils/cmc/exception/CMCException.java
+++ utils/src/main/java/uk/gov/moj/sdt/utils/cmc/exception/CMCException.java
@@ -2,7 +2,6 @@ package uk.gov.moj.sdt.utils.cmc.exception;
 
 public class CMCException extends RuntimeException {
 
-    @java.io.Serial
     private static final long serialVersionUID = 1921937543087682070L;
 
     public CMCException(String message) {
