a8af1a3ee445040030628c7ba7079c8de8c46f7e
ridoutp <paul.ridout@cgi.com>
2023-03-10 15:08:13 +0000

    SDT-12: Extract customer IDAM id from SOAP message
    - Added IdamIdInboundInterceptor to extract customer IDAM id from incoming SOAP message and store in SDT context.
    - Added customerIdamId field to SdtContext.
    - Tidied up SdtContextTest.
    - Added IdamIdInboundInterceptor to sdtEndpoint bean method in CxfConfig.
    - Added setAccessibleField() method to AbstractSdtUnitTestBase.  Reworked getAccessibleField() to avoid possible null values.
    - Added FieldAccessException to be used by get/set accessible methods on AbstractSdtUnitTestBase.

---
+++ utils/src/unit-test/java/uk/gov/moj/sdt/utils/exception/FieldAccessException.java
@@ -0,0 +1,9 @@
+package uk.gov.moj.sdt.utils.exception;
+
+public class FieldAccessException extends RuntimeException {
+
+    public FieldAccessException(String message) {
+        super(message);
+    }
+
+}
