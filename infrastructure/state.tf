f683628bf2a498d75ede5ea95ba7bcd384346698
ridoutp <paul.ridout@cgi.com>
2023-03-20 10:19:17 +0000

    Updated Terraform configuration
    - Removed duplicate azurerm provider declaration from state.tf
    - Removed civil-shared resource declaration from main.tf.  This is already being created in the civil-shared-infrastructure Terraform configuration.  Replaced it with a new local civil_shared_resource_group variable.
    - Replaced references to shared resource group with civil_shared_resource_group variable in key-vault.tf and service-bus.tf.
    - Moved output variables from key-vault.tf and service-bus.tf to output.tf.

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-}
-
-provider "azurerm" {
-  features {}
   skip_provider_registration = true
   alias                      = "private_endpoint"
   subscription_id            = var.aks_subscription_id
