205fa06e006c8d7ae3ea62382bf648d9334db4b5
markdathorne <mark.dathorne@cgi.com>
2023-03-30 15:05:38 +0100

    SDT-79 PurgeService code coverage unit test

---
+++ services/src/unit-test/java/uk/gov/moj/sdt/services/PurgeServiceTest.java
@@ -0,0 +1,39 @@
+package uk.gov.moj.sdt.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.moj.sdt.dao.PurgeNativeCallFunction;
+import uk.gov.moj.sdt.utils.AbstractSdtUnitTestBase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class PurgeServiceTest extends AbstractSdtUnitTestBase {
+
+    private PurgeService purgeService;
+    @Mock
+    PurgeNativeCallFunction purgeNativeCallFunction;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        purgeService = new PurgeService(purgeNativeCallFunction);
+    }
+
+    @Test
+    void shouldPerformTransactionalPurge() {
+        purgeService.transactPurgeProc();
+        assertTrue(true, "Expected to pass");
+    }
+
+    @Test
+    void shouldRetrieveDefaultCommitInterval() {
+        Integer expectedValue = 500;
+        Integer commitInterval =  purgeService.getCommitInterval();
+        assertEquals(expectedValue, commitInterval, "commitInterval parameter is not expected value");
+    }
+}
