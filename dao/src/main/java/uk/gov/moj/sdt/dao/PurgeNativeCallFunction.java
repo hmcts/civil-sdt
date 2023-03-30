994600c5ee4bfa12fbcd27f003842017228edcbc
markdathorne <mark.dathorne@cgi.com>
2023-03-30 15:16:22 +0100

    SDT-79 Remove extra blank line

--- dao/src/main/java/uk/gov/moj/sdt/dao/PurgeNativeCallFunction.java
+++ dao/src/main/java/uk/gov/moj/sdt/dao/PurgeNativeCallFunction.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
