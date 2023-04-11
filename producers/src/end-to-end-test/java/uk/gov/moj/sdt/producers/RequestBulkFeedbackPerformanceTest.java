ecb3bcbc828bd6f697b4823b3abb156b7d6fbbfc
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-04-11 23:42:21 +0100

    SDT-85: resolved gradle build issues

--- producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/RequestBulkFeedbackPerformanceTest.java
+++ producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/RequestBulkFeedbackPerformanceTest.java
@@ -6,17 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.moj.sdt.test.utils.DBUnitUtilityBean;
-import uk.gov.moj.sdt.test.utils.TestConfig;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackrequestschema.BulkFeedbackRequestType;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackresponseschema.BulkFeedbackResponseType;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackresponseschema.ObjectFactory;
 import uk.gov.moj.sdt.ws._2013.sdt.sdtendpoint.ISdtEndpointPortType;
-import uk.gov.moj.sdt.ws._2013.sdt.sdtendpoint.SdtEndpoint;
-import uk.gov.moj.sdt.ws._2013.sdt.sdtinternalendpoint.SdtInternalEndpoint;
 
 /**
  * End to end performance test for bulk feedback web service.
@@ -27,8 +21,6 @@ import uk.gov.moj.sdt.ws._2013.sdt.sdtinternalendpoint.SdtInternalEndpoint;
 @ActiveProfiles("integ")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = { EndToEndTestConfig.class, DaoEndToEndTestConfig.class})
-//@ContextConfiguration(locations = {"classpath*:/uk/gov/moj/sdt/producers/spring*e2e.test.xml",
-//        "classpath*:/uk/gov/moj/sdt/utils/**/spring*.xml", "classpath*:/uk/gov/moj/sdt/transformers/**/spring*.xml"})
 public class RequestBulkFeedbackPerformanceTest extends
         AbstractWebServiceTest<BulkFeedbackRequestType, BulkFeedbackResponseType> {
     /**
