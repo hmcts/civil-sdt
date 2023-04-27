885832e64940b31203969faac3fe9cd4fe7ed5ea
markdathorne <mark.dathorne@cgi.com>
2023-04-27 16:04:14 +0100

    SDT-85 change comma pos fpr easy debugging, switching off SQL scripts.

--- producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/RequestBulkFeedbackPerformanceTest.java
+++ producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/RequestBulkFeedbackPerformanceTest.java
@@ -23,15 +23,15 @@ import uk.gov.moj.sdt.ws._2013.sdt.sdtendpoint.ISdtEndpointPortType;
 @ActiveProfiles("end-to-end-test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = { EndToEndTestConfig.class })
-@Sql(scripts = {"classpath:database/baseline/drop_and_recreate_empty_public_schema.sql",
-        "classpath:database/baseline/V0001__init.sql",
-        "classpath:database/baseline/V0003__alter_bulk_customer.sql",
-        "classpath:database/baseline/V0004__alter_individual_request.sql",
-        "classpath:database/baseline/V0005__alter_individual_request.sql",
-        "classpath:database/baseline/create_purge_proc.sql",
-        "classpath:database/baseline/create_finish_dbunit_load_proc.sql",
-        "classpath:database/baseline/create_prepare_for_dbunit_load_proc.sql",
-        "classpath:database/baseline/RequestBulkFeedbackPerformanceTest.sql",
+@Sql(scripts = {"classpath:database/baseline/drop_and_recreate_empty_public_schema.sql"
+        ,"classpath:database/baseline/V0001__init.sql"
+        ,"classpath:database/baseline/V0003__alter_bulk_customer.sql"
+        ,"classpath:database/baseline/V0004__alter_individual_request.sql"
+        ,"classpath:database/baseline/V0005__alter_individual_request.sql"
+        ,"classpath:database/baseline/create_purge_proc.sql"
+        ,"classpath:database/baseline/create_finish_dbunit_load_proc.sql"
+        ,"classpath:database/baseline/create_prepare_for_dbunit_load_proc.sql"
+        ,"classpath:database/baseline/RequestBulkFeedbackPerformanceTest.sql"
 })
 public class RequestBulkFeedbackPerformanceTest extends
         AbstractWebServiceTest<BulkFeedbackRequestType, BulkFeedbackResponseType> {
