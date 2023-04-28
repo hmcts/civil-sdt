189e8e3aa556f218ff3e1760b2701f407d24f1b8
markdathorne <mark.dathorne@cgi.com>
2023-04-28 06:33:21 +0100

    SDT-85 SubmitBulkTest now uses SQL startup scripts instead of DBUnit XML update. Error Result expected message check corrected.

--- producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/SubmitBulkTest.java
+++ producers/src/end-to-end-test/java/uk/gov/moj/sdt/producers/SubmitBulkTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import uk.gov.moj.sdt.producers.config.EndToEndTestConfig;
+import uk.gov.moj.sdt.producers.config.SecurityConfig;
 import uk.gov.moj.sdt.test.utils.DBUnitUtilityBean;
 import uk.gov.moj.sdt.utils.SpringApplicationContext;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkrequestschema.BulkRequestType;
@@ -29,14 +30,17 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 @ActiveProfiles("end-to-end-test")
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = { EndToEndTestConfig.class })
-@Sql(scripts = {"classpath:database/baseline/drop_and_recreate_empty_public_schema.sql",
-        "classpath:database/baseline/V0001__init.sql",
-        "classpath:database/baseline/V0003__alter_bulk_customer.sql",
-        "classpath:database/baseline/V0004__alter_individual_request.sql",
-        "classpath:database/baseline/create_purge_proc.sql",
-        "classpath:database/baseline/create_finish_dbunit_load_proc.sql",
-        "classpath:database/baseline/create_prepare_for_dbunit_load_proc.sql"})
+@SpringBootTest(classes = { EndToEndTestConfig.class, SecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql(scripts = {"classpath:database/baseline/drop_and_recreate_empty_public_schema.sql"
+        ,"classpath:database/baseline/V0001__init.sql"
+        ,"classpath:database/baseline/V0003__alter_bulk_customer.sql"
+        ,"classpath:database/baseline/V0004__alter_individual_request.sql"
+        ,"classpath:database/baseline/V0005__alter_individual_request.sql"
+        ,"classpath:database/baseline/create_purge_proc.sql"
+        ,"classpath:database/baseline/create_finish_dbunit_load_proc.sql"
+        ,"classpath:database/baseline/create_prepare_for_dbunit_load_proc.sql"
+        ,"classpath:database/baseline/SubmitBulkTest.sql"
+})
 public class SubmitBulkTest extends AbstractWebServiceTest<BulkRequestType, BulkResponseType> {
     /**
      * Thirty seconds in milliseconds.
