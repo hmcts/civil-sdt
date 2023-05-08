50e8c2405903b247fe03ef1c4ea9707dcfe6fc60
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-05-08 22:07:50 +0100

    SDT-40: Update existing Judgement and Warrant Request combined workflow

---
+++ consumers/src/unit-test/java/uk/gov/moj/sdt/cmc/consumers/client/mock/MockJudgementWarrantServiceTest.java
@@ -0,0 +1,45 @@
+package uk.gov.moj.sdt.cmc.consumers.client.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.moj.sdt.cmc.consumers.request.JudgementWarrantRequest;
+import uk.gov.moj.sdt.cmc.consumers.response.JudgementWarrantResponse;
+import uk.gov.moj.sdt.utils.AbstractSdtUnitTestBase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+public class MockJudgementWarrantServiceTest extends AbstractSdtUnitTestBase {
+
+    String AUTHORISATION = "";
+
+    String SERVICE_AUTHORISATION = "";
+
+    String IDAM_ID_HEADER = "IDAMID";
+
+    String SDT_REQUEST_ID = "SDTREQUESTID";
+
+    private MockJudgementWarrantService mockJudgementWarrantService;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        mockJudgementWarrantService = new MockJudgementWarrantService();
+    }
+
+    @Test
+    void getClient() {
+        JudgementWarrantRequest request = mock(JudgementWarrantRequest.class);
+        JudgementWarrantResponse response = mockJudgementWarrantService.judgementWarrantRequest(AUTHORISATION,
+                                                                                                SERVICE_AUTHORISATION,
+                                                                                                IDAM_ID_HEADER,
+                                                                                                SDT_REQUEST_ID , request);
+        assertNotNull(response);
+        assertEquals(45678L, response.getFee());
+        assertEquals("123456", response.getWarrantNumber());
+    }
+}
