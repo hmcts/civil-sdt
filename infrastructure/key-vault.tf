60fc929ea11cd83ccf36d7ff5d904ce7990bcb4a
ridoutp <paul.ridout@cgi.com>
2023-05-11 13:54:01 +0100

    Add database setup to infrastructure code
    - Added managed_identity_object_ids parameter to key vault.

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -1,13 +1,19 @@
+data "azurerm_user_assigned_identity" "civil-mi" {
+  name                = "${var.product}-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "civil_sdt_key_vault" {
   source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
 
-  name                    = "${var.product}-${var.component}-${var.env}"
-  product                 = var.product
-  env                     = var.env
-  object_id               = var.jenkins_AAD_objectId
-  resource_group_name     = azurerm_resource_group.civil_sdt_rg.name
-  product_group_name      = "DTS Civil"
-  common_tags             = local.tags
+  name                        = "${var.product}-${var.component}-${var.env}"
+  product                     = var.product
+  env                         = var.env
+  object_id                   = var.jenkins_AAD_objectId
+  resource_group_name         = azurerm_resource_group.civil_sdt_rg.name
+  product_group_name          = "DTS Civil"
+  common_tags                 = local.tags
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.civil-mi.principal_id]
 }
 
 data "azurerm_key_vault" "s2s_vault" {
