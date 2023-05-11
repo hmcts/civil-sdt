d65e8914008816114c6726e466b120a716f434a2
markdathorne <mark.dathorne@cgi.com>
2023-05-11 10:25:41 +0100

    SDT-85 Allow for both single AND null BulkSubmission result to be returned

--- dao/src/main/java/uk/gov/moj/sdt/dao/BulkSubmissionDao.java
+++ dao/src/main/java/uk/gov/moj/sdt/dao/BulkSubmissionDao.java
@@ -1,33 +1,3 @@
-/* Copyrights and Licenses
- *
- * Copyright (c) 2013 by the Ministry of Justice. All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this list of conditions
- * and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * - All advertising materials mentioning features or use of this software must display the
- * following acknowledgment: "This product includes Money Claims OnLine."
- * - Products derived from this software may not be called "Money Claims OnLine" nor may
- * "Money Claims OnLine" appear in their names without prior written permission of the
- * Ministry of Justice.
- * - Redistributions of any form whatsoever must retain the following acknowledgment: "This
- * product includes Money Claims OnLine."
- * This software is provided "as is" and any expressed or implied warranties, including, but
- * not limited to, the implied warranties of merchantability and fitness for a particular purpose are
- * disclaimed. In no event shall the Ministry of Justice or its contributors be liable for any
- * direct, indirect, incidental, special, exemplary, or consequential damages (including, but
- * not limited to, procurement of substitute goods or services; loss of use, data, or profits;
- * or business interruption). However caused any on any theory of liability, whether in contract,
- * strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this
- * software, even if advised of the possibility of such damage.
- *
- * $Id: $
- * $LastChangedRevision: $
- * $LastChangedDate: $
- * $LastChangedBy: $ */
 package uk.gov.moj.sdt.dao;
 
 import org.slf4j.Logger;
@@ -42,6 +12,7 @@ import uk.gov.moj.sdt.domain.api.IBulkCustomer;
 import uk.gov.moj.sdt.domain.api.IBulkSubmission;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -93,15 +64,22 @@ public class BulkSubmissionDao extends GenericDao<BulkSubmission> implements IBu
             criteriaBuilder.lower(root.get("customerReference")),
             customerReference.toLowerCase()
         );
+        Predicate sdtDatePredicate = createDatePredicate(criteriaBuilder, root, dataRetention);
+
         TypedQuery<BulkSubmission> typedQuery = getEntityManager().createQuery(criteriaQuery.select(root)
                                                                                    .where(
-                                                                                       sdtCustomerPredicate,
-                                                                                       customerReferencePredicate,
-                                                                                       createDatePredicate(criteriaBuilder, root, dataRetention)
+                                                                                       sdtCustomerPredicate
+                                                                                       ,customerReferencePredicate
+                                                                                       ,sdtDatePredicate
                                                                                    ));
 
+        LOGGER.debug("typedQuery.getResultList().size() == {}", typedQuery.getResultList().size());
 
-        return typedQuery.getSingleResult();
+        try {
+            return typedQuery.getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
     }
 
     @Override
@@ -120,14 +98,21 @@ public class BulkSubmissionDao extends GenericDao<BulkSubmission> implements IBu
             criteriaBuilder.lower(root.get("sdtBulkReference")),
             sdtBulkReference.toLowerCase()
         );
+        Predicate sdtDatePredicate = createDatePredicate(criteriaBuilder, root, dataRetention);
 
         TypedQuery<BulkSubmission> typedQuery = getEntityManager().createQuery(criteriaQuery.select(root)
                 .where(
-                        sdtCustomerPredicate,
-                        sdtBulkRefPredicate,
-                        createDatePredicate(criteriaBuilder, root, dataRetention)
+                        sdtCustomerPredicate
+                        ,sdtBulkRefPredicate
+                        ,sdtDatePredicate
                 ));
+
         LOGGER.debug("typedQuery.getResultList().size() == {}", typedQuery.getResultList().size());
-        return typedQuery.getSingleResult();
+
+        try {
+            return typedQuery.getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
     }
 }
