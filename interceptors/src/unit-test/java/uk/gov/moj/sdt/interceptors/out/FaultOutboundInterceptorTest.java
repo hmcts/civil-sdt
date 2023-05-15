b72e1029b9031cda6a8c69c2478e5cc5470ada0b
markdathorne <mark.dathorne@cgi.com>
2023-05-15 11:13:13 +0100

    SDT-85 rename no longer mock instance of requestDaoService. Sonarlint fixes

--- interceptors/src/unit-test/java/uk/gov/moj/sdt/interceptors/out/FaultOutboundInterceptorTest.java
+++ interceptors/src/unit-test/java/uk/gov/moj/sdt/interceptors/out/FaultOutboundInterceptorTest.java
@@ -1,39 +1,4 @@
-/* Copyrights and Licenses
- *
- * Copyright (c) 2012-2014 by the Ministry of Justice. All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this list of conditions
- * and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * - All advertising materials mentioning features or use of this software must display the
- * following acknowledgment: "This product includes Money Claims OnLine."
- * - Products derived from this software may not be called "Money Claims OnLine" nor may
- * "Money Claims OnLine" appear in their names without prior written permission of the
- * Ministry of Justice.
- * - Redistributions of any form whatsoever must retain the following acknowledgment: "This
- * product includes Money Claims OnLine."
- * This software is provided "as is" and any expressed or implied warranties, including, but
- * not limited to, the implied warranties of merchantability and fitness for a particular purpose are
- * disclaimed. In no event shall the Ministry of Justice or its contributors be liable for any
- * direct, indirect, incidental, special, exemplary, or consequential damages (including, but
- * not limited to, procurement of substitute goods or services; loss of use, data, or profits;
- * or business interruption). However caused any on any theory of liability, whether in contract,
- * strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this
- * software, even if advised of the possibility of such damage.
- *
- * $Id: $
- * $LastChangedRevision: $
- * $LastChangedDate: $
- * $LastChangedBy: $ */
-/**
- * Test this class handles faults correctly.
- */
 package uk.gov.moj.sdt.interceptors.out;
-
-import java.nio.charset.StandardCharsets;
 
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.interceptor.Fault;
@@ -52,6 +17,8 @@ import uk.gov.moj.sdt.interceptors.service.RequestDaoService;
 import uk.gov.moj.sdt.utils.AbstractSdtUnitTestBase;
 import uk.gov.moj.sdt.utils.SdtContext;
 import uk.gov.moj.sdt.utils.logging.PerformanceLogger;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -72,7 +39,7 @@ class FaultOutboundInterceptorTest extends AbstractSdtUnitTestBase {
     @Mock
     ServiceRequestDao mockServiceRequestDao;
 
-    RequestDaoService mockRequestDaoService;
+    RequestDaoService requestDaoService;
 
     /**
      * Error message returned when a fault occurs.
@@ -81,8 +48,10 @@ class FaultOutboundInterceptorTest extends AbstractSdtUnitTestBase {
             "has occurred. Please contact the SDT support team for assistance";
 
     @BeforeEach
-    void setup() {
-        mockRequestDaoService = new RequestDaoService(mockServiceRequestDao);
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        requestDaoService = new RequestDaoService(mockServiceRequestDao);
     }
 
     /**
@@ -94,7 +63,7 @@ class FaultOutboundInterceptorTest extends AbstractSdtUnitTestBase {
     void testHandleMessage() {
         SdtContext.getContext().setServiceRequestId(1L);
         final SoapMessage soapMessage = getDummySoapMessageWithFault();
-        final FaultOutboundInterceptor faultOutboundInterceptor = new FaultOutboundInterceptor(mockRequestDaoService);
+        final FaultOutboundInterceptor faultOutboundInterceptor = new FaultOutboundInterceptor(requestDaoService);
         final ServiceRequest serviceRequest = new ServiceRequest();
         when(mockServiceRequestDao.fetch(ServiceRequest.class, 1L)).thenReturn(serviceRequest);
         mockServiceRequestDao.persist(serviceRequest);
@@ -111,7 +80,7 @@ class FaultOutboundInterceptorTest extends AbstractSdtUnitTestBase {
     void testHandleMessageLogOutput() {
         SdtContext.getContext().setServiceRequestId(1L);
         final SoapMessage soapMessage = getDummySoapMessageWithFault();
-        final FaultOutboundInterceptor faultOutboundInterceptor = new FaultOutboundInterceptor(mockRequestDaoService);
+        final FaultOutboundInterceptor faultOutboundInterceptor = new FaultOutboundInterceptor(requestDaoService);
         final ServiceRequest serviceRequest = new ServiceRequest();
         when(mockServiceRequestDao.fetch(ServiceRequest.class, 1L)).thenReturn(serviceRequest);
         mockServiceRequestDao.persist(serviceRequest);
