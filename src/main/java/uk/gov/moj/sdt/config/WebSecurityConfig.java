732bd6df2a69825261ef6c48e71e0b8142d1f904
markdathorne <mark.dathorne@cgi.com>
2023-05-15 11:37:57 +0100

    SDT-85 Spring Security compliance

--- src/main/java/uk/gov/moj/sdt/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/moj/sdt/config/WebSecurityConfig.java
@@ -17,9 +17,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-            .authorizeHttpRequests()
-            .anyRequest().permitAll();
+//        http.csrf()
+//            .disable()
+//            .authorizeHttpRequests()
+//            .anyRequest().permitAll();
 
         return http.build();
     }
