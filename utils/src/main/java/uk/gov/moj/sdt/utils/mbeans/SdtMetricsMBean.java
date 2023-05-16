851109246230801960b35ac9067e55cd2f615d8c
markdathorne <mark.dathorne@cgi.com>
2023-05-16 09:47:00 +0100

    SDT-85 remove unused autowired bean

--- utils/src/main/java/uk/gov/moj/sdt/utils/mbeans/SdtMetricsMBean.java
+++ utils/src/main/java/uk/gov/moj/sdt/utils/mbeans/SdtMetricsMBean.java
@@ -1,40 +1,7 @@
-/* Copyrights and Licenses
- *
- * Copyright (c) 2013 by the Ministry of Justice. All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this list of conditions
- * and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * - All advertising materials mentioning features or use of this software must display the
- * following acknowledgment: "This product includes Money Claims OnLine."
- * - Products derived from this software may not be called "Money Claims OnLine" nor may
- * "Money Claims OnLine" appear in their names without prior written permission of the
- * Ministry of Justice.
- * - Redistributions of any form whatsoever must retain the following acknowledgment: "This
- * product includes Money Claims OnLine."
- * This software is provided "as is" and any expressed or implied warranties, including, but
- * not limited to, the implied warranties of merchantability and fitness for a particular purpose are
- * disclaimed. In no event shall the Ministry of Justice or its contributors be liable for any
- * direct, indirect, incidental, special, exemplary, or consequential damages (including, but
- * not limited to, procurement of substitute goods or services; loss of use, data, or profits;
- * or business interruption). However caused any on any theory of liability, whether in contract,
- * strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this
- * software, even if advised of the possibility of such damage.
- *
- * $Id: $
- * $LastChangedRevision: $
- * $LastChangedDate: $
- * $LastChangedBy: $ */
-
 package uk.gov.moj.sdt.utils.mbeans;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import uk.gov.moj.sdt.utils.logging.PerformanceLogger;
 import uk.gov.moj.sdt.utils.mbeans.api.ICustomerCounter;
@@ -412,12 +379,6 @@ public final class SdtMetricsMBean implements ISdtMetricsMBean {
      * Utility class for counting unique customers.
      */
     private ICustomerCounter customerCounter;
-
-    @Autowired
-    public SdtMetricsMBean(@Qualifier("CustomerCounter")
-                               ICustomerCounter customerCounter) {
-        this.customerCounter = customerCounter;
-    }
 
     /**
      * Constructor for {@link SdtMetricsMBean}. This called by Spring and should become the bean that all subsequent
@@ -1378,12 +1339,14 @@ public final class SdtMetricsMBean implements ISdtMetricsMBean {
     @Override
     public String getBulkSubmitStats() {
 
-        return "Bulk submits: count[" + this.getBulkSubmitCount() + "], last["
-                + this.formatter.format(bulkSubmitLastTime) + "], rate["
-                + this.getRate(this.getBulkSubmitCount(), this.getElapsedTime()) + "], requests["
-                + this.getRequestCount() + "], rate[" + this.getRate(this.getRequestCount(), this.getElapsedTime())
-                + "], time[" + this.getBulkSubmitTime() + "], average[" + this.getBulkSubmitTimeAvg() + "], minimum["
-                + this.getBulkSubmitTimeMin() + "], maximum[" + this.getBulkSubmitTimeMax() + "]";
+        return new StringBuilder().append("Bulk submits: count[").append(this.getBulkSubmitCount()).append("], last[")
+                .append(this.formatter.format(bulkSubmitLastTime)).append("], rate[")
+                .append(this.getRate(this.getBulkSubmitCount(), this.getElapsedTime())).append("], requests[")
+                .append(this.getRequestCount()).append("], rate[").append(this.getRate(this.getRequestCount(),
+                        this.getElapsedTime())).append("], time[").append(this.getBulkSubmitTime())
+                .append("], average[").append(this.getBulkSubmitTimeAvg()).append("], minimum[")
+                .append(this.getBulkSubmitTimeMin()).append("], maximum[").append(this.getBulkSubmitTimeMax())
+                .append("]").toString();
     }
 
     @Override
