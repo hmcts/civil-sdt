0a3746a5122dd857fc158212ce9d1e96ab764dab
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-13 22:57:40 +0100

    Fix S2S issue

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -3,17 +3,9 @@ data "azurerm_user_assigned_identity" "civil-mi" {
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
-module "civil_sdt_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-
-  name                        = "${var.product}-${var.component}-${var.env}"
-  product                     = var.product
-  env                         = var.env
-  object_id                   = var.jenkins_AAD_objectId
-  resource_group_name         = azurerm_resource_group.civil_sdt_rg.name
-  product_group_name          = "DTS Civil"
-  common_tags                 = local.tags
-  managed_identity_object_ids = [data.azurerm_user_assigned_identity.civil-mi.principal_id]
+data "azurerm_key_vault" "key_vault" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
 }
 
 data "azurerm_key_vault" "s2s_vault" {
@@ -28,7 +20,7 @@ data "azurerm_key_vault_secret" "key_from_vault" {
 }
 
 resource "azurerm_key_vault_secret" "s2s" {
-  name         = "civil-sdt-service-s2s-secret"
+  name         = "s2s-secret"
   value        = data.azurerm_key_vault_secret.key_from_vault.value
-  key_vault_id = module.civil_sdt_key_vault.key_vault_id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
