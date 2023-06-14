95cf6f0198d1b5c353ba936891a729948b2b8bb5
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-14 11:08:31 +0100

    change civil-sdt keyvalue to module

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -3,9 +3,17 @@ data "azurerm_user_assigned_identity" "civil-mi" {
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
-data "azurerm_key_vault" "key_vault" {
-  name                = "${var.product}-${var.component}-${var.env}"
-  resource_group_name = "${var.product}-${var.component}-${var.env}"
+module "civil_sdt_key_vault" {
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+
+  name                        = "${var.product}-${var.component}-${var.env}"
+  product                     = var.product
+  env                         = var.env
+  object_id                   = var.jenkins_AAD_objectId
+  resource_group_name         = azurerm_resource_group.civil_sdt_rg.name
+  product_group_name          = "DTS Civil"
+  common_tags                 = local.tags
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.civil-mi.principal_id]
 }
 
 data "azurerm_key_vault" "s2s_vault" {
@@ -21,5 +29,5 @@ data "azurerm_key_vault_secret" "key_from_vault" {
 resource "azurerm_key_vault_secret" "s2s" {
   name         = "civil-sdt-service-s2s-secret"
   value        = data.azurerm_key_vault_secret.key_from_vault.value
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  key_vault_id = module.civil_sdt_key_vault.key_vault_id
 }
