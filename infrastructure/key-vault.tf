273cb0f8364ae5690621c0e62a2dc2aa33ab7193
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-14 09:51:19 +0100

    civil-sdt-service-s2s-secret added

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -19,7 +19,7 @@ data "azurerm_key_vault_secret" "key_from_vault" {
 }
 
 resource "azurerm_key_vault_secret" "s2s" {
-  name         = "s2s-secret"
+  name         = "civil-sdt-service-s2s-secret"
   value        = data.azurerm_key_vault_secret.key_from_vault.value
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
