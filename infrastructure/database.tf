9ece42a51f4492b2d4292537829aa650f2eb87d5
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-10-02 15:04:04 +0100

    SDT-168: SDT: Allow database retention period to be specified on an environment basis

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -7,19 +7,19 @@ module "postgresql" {
     azurerm.postgres_network = azurerm.private_endpoint
   }
 
-  admin_user_object_id = var.jenkins_AAD_objectId
-  business_area        = "cft"
-  common_tags          = local.tags
-  component            = var.component
-  env                  = var.env
+  admin_user_object_id  = var.jenkins_AAD_objectId
+  business_area         = "cft"
+  common_tags           = local.tags
+  component             = var.component
+  env                   = var.env
   pgsql_databases = [
     {
       name = "civil_sdt"
     }
   ]
-  pgsql_version        = "11"
-  product              = var.product
-  backup_retention_days= "${var.database_backup_retention_days}"
+  pgsql_version         = "11"
+  product               = var.product
+  backup_retention_days = "${var.database_backup_retention_days}"
 }
 
 # Create secret for database user
