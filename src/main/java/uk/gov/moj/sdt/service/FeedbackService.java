af006052c1cd835cb0e7ae1c284139235d2efdc7
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-11-09 11:10:44 +0000

    SDT-146: Asynchronous Endpoint for CMC

--- src/main/java/uk/gov/moj/sdt/service/FeedbackService.java
+++ src/main/java/uk/gov/moj/sdt/service/FeedbackService.java
@@ -2,8 +2,11 @@ package uk.gov.moj.sdt.service;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import uk.gov.moj.sdt.cmc.consumers.response.ProcessingStatus;
 import uk.gov.moj.sdt.dao.api.IIndividualRequestDao;
+import uk.gov.moj.sdt.domain.ErrorLog;
+import uk.gov.moj.sdt.domain.api.IErrorLog;
 import uk.gov.moj.sdt.domain.api.IIndividualRequest;
 import uk.gov.moj.sdt.request.CMCFeedback;
 import uk.gov.moj.sdt.utils.cmc.RequestType;
@@ -21,19 +24,63 @@ public class FeedbackService {
     public void feedback(String sdtRequestId, CMCFeedback cmcFeedback) {
         IIndividualRequest individualRequest = individualRequestDao.getRequestBySdtReference(sdtRequestId);
         String requestType = individualRequest.getRequestType();
-        ProcessingStatus processingStatus = ProcessingStatus.valueOf(cmcFeedback.getProcessingStatus());
         if (RequestType.JUDGMENT.getType().equals(requestType)) {
+            updateJudgementRequest(cmcFeedback, individualRequest);
         } else if (RequestType.BREATHING_SPACE.getType().equals(requestType)) {
-            individualRequest.setRequestStatus(cmcFeedback.getProcessingStatus());
+            updateBreathingSpaceRequest(cmcFeedback, individualRequest);
         } else if (RequestType.CLAIM_STATUS_UPDATE.getType().equals(requestType)) {
-            individualRequest.setRequestStatus(processingStatus.name());
+            updateClaimStatusUpdateRequest(cmcFeedback, individualRequest);
         } else if (RequestType.CLAIM.getType().equals(requestType)) {
-            individualRequest.setTargetApplicationResponse(null);
+            updateCreateClaimRequest(cmcFeedback, individualRequest);
         } else if (RequestType.WARRANT.getType().equals(requestType)) {
-            individualRequest.setTargetApplicationResponse(null);
+            updateWarrantRequest(cmcFeedback, individualRequest);
         } else if (RequestType.JUDGMENT_WARRANT.getType().equals(requestType)) {
-            individualRequest.setTargetApplicationResponse(null);
+            updateCombinedJudgementWarrantRequest(cmcFeedback, individualRequest);
         }
         individualRequestDao.persist(individualRequest);
+    }
+
+    private void updateCreateClaimRequest(CMCFeedback cmcFeedback,
+                                          IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+
+    }
+
+    private void updateJudgementRequest(CMCFeedback cmcFeedback,
+                                          IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+    }
+
+    private void updateWarrantRequest(CMCFeedback cmcFeedback,
+                                        IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+    }
+
+    private void updateCombinedJudgementWarrantRequest(CMCFeedback cmcFeedback,
+                                      IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+    }
+
+    private void updateClaimStatusUpdateRequest(CMCFeedback cmcFeedback,
+                                      IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+    }
+
+    private void updateBreathingSpaceRequest(CMCFeedback cmcFeedback,
+                                                IIndividualRequest individualRequest) {
+        addStatusAndErrorLog(cmcFeedback, individualRequest);
+    }
+
+    private void addStatusAndErrorLog(CMCFeedback cmcFeedback, IIndividualRequest individualRequest) {
+        if (StringUtils.hasText(cmcFeedback.getErrorText())) {
+            final IErrorLog errorLog = new ErrorLog(String.valueOf(cmcFeedback.getErrorCode()), cmcFeedback.getErrorText());
+            individualRequest.setErrorLog(errorLog);
+        }
+        if (StringUtils.hasText(cmcFeedback.getProcessingStatus())) {
+            ProcessingStatus processingStatus = ProcessingStatus.valueOf(cmcFeedback.getProcessingStatus());
+            if (processingStatus == ProcessingStatus.PROCESSED) {
+                individualRequest.setRequestStatus(processingStatus.name());
+            }
+        }
     }
 }
