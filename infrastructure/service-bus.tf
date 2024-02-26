a3a2435fe7f64cb944645335c11d01efb9205a4b
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-02-26 13:23:39 +0000

    Change module ref

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -2,7 +2,7 @@ module "servicebus-namespace" {
   providers = {
     azurerm.private_endpoint = azurerm.private_endpoint
   }
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=adding-premium-messaging-partitions"
   name                = "${var.product}-${var.component}-servicebus-${var.env}"
   resource_group_name = azurerm_resource_group.civil_sdt_rg.name
   location            = var.location
