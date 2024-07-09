ad8f93f578a6f9475ab5fcb0aa5ec5497f4afdab
ridoutp <paul.ridout@cgi.com>
2024-07-09 11:10:11 +0100

    SDT-227: Set timezone to Europe/London
    Added PostConstruct annotated method to Application.java to explicitly set the timezone to Europe/London.

--- src/main/java/uk/gov/moj/sdt/Application.java
+++ src/main/java/uk/gov/moj/sdt/Application.java
@@ -5,14 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages={"uk.gov.moj.sdt"})
 @EnableJpaRepositories(basePackages={"uk.gov.moj.sdt"})
 @EnableTransactionManagement
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
 public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
     }
 }
