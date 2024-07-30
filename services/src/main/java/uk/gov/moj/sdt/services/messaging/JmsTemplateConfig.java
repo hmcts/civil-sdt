1076274fd728605df9408312b2608a720d93ec10
rebeccabaker <rebecca.baker@hmcts.net>
2024-07-30 13:29:12 +0100

    update JMS Template to be behind feature toggle

--- services/src/main/java/uk/gov/moj/sdt/services/messaging/JmsTemplateConfig.java
+++ services/src/main/java/uk/gov/moj/sdt/services/messaging/JmsTemplateConfig.java
@@ -1,19 +1,15 @@
 package uk.gov.moj.sdt.services.messaging;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.jms.ConnectionFactory;
 import javax.jms.Session;
 
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.jms.servicebus.internal")
 @ConditionalOnProperty("enable-jms")
 public class JmsTemplateConfig {
 
