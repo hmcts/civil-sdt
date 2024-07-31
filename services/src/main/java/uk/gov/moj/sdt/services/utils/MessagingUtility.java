7d96dfcea8dd5384d60e80db41cc8e2b0d57e5dd
ridoutp <paul.ridout@cgi.com>
2024-07-31 14:21:46 +0100

    Add executor service to single request enqueueRequest method

--- services/src/main/java/uk/gov/moj/sdt/services/utils/MessagingUtility.java
+++ services/src/main/java/uk/gov/moj/sdt/services/utils/MessagingUtility.java
@@ -66,6 +66,9 @@ public class MessagingUtility implements IMessagingUtility {
     @Value("${sdt.service.config.queue_delay:500}")
     private int queueDelay;
 
+    @Value("${enable-single-executor:false}")
+    private boolean enableSingleExecutor;
+
     private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     @Autowired
@@ -77,14 +80,23 @@ public class MessagingUtility implements IMessagingUtility {
     @Override
     public void enqueueRequest(final IIndividualRequest individualRequest) {
         LOGGER.debug("enqueueRequest: [{}]", individualRequest.getSdtRequestReference());
-        queueRequest(individualRequest);
+        if (enableSingleExecutor) {
+            executorService.schedule(() -> {
+                LOGGER.debug("enqueueRequests: executorService (single)");
+                queueRequest(individualRequest);
+                }
+                , queueDelay, TimeUnit.MILLISECONDS
+            );
+        } else {
+            queueRequest(individualRequest);
+        }
     }
 
     @Override
     public void enqueueRequests(List<IIndividualRequest> individualRequests) {
         LOGGER.debug("enqueueRequests: Number of requests [{}]", individualRequests.size());
         executorService.schedule(() -> {
-            LOGGER.debug("enqueueRequests: executorService");
+            LOGGER.debug("enqueueRequests: executorService (multiple)");
             queueRequest(individualRequests);
             }
             , queueDelay, TimeUnit.MILLISECONDS
